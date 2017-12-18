7d7043370bb596834141a5fe3151e6c013678bb0
William Carter <willcarter@Williams-MacBook-Pro.local>
2017-12-18 15:28:28 -0800

    added io.Reader to the Gen struct so it isn't created on every Rand call

--- cyclic/generator.go
+++ cyclic/generator.go
@@ -1,29 +1,33 @@
 package cyclic
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"io"
+)
 
 type Gen struct {
-	min *Int
-	max *Int
+	min    *Int
+	max    *Int
+	reader io.Reader
 }
 
 // Initialize a new Gen with min and max values
 func NewGen(min, max *Int) Gen {
-	gen := Gen{min, max}
+	gen := Gen{min, max, rand.Reader}
 	return gen
 }
 
 // Generates a random Int between min and max, performance degrades
 // as min approaches max
 func (gen Gen) Rand(x *Int) *Int {
-	ran, err := rand.Int(rand.Reader, gen.max.value)
+	ran, err := rand.Int(gen.reader, gen.max.value)
 	if err != nil {
 		return nil
 	}
 	x.value = ran
 
 	for x.Cmp(gen.min) < 0 {
-		ran, err := rand.Int(rand.Reader, gen.max.value)
+		ran, err := rand.Int(gen.reader, gen.max.value)
 		if err != nil {
 			return nil
 		}
