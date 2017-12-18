6d012c030bd0655d2117aee6c3344cdb7ed633b8
William Carter <willcarter@Williams-MacBook-Pro.local>
2017-12-18 15:53:54 -0800

    improved random generator performance for high min values

--- cyclic/generator.go
+++ cyclic/generator.go
@@ -8,30 +8,24 @@ import (
 type Gen struct {
 	min    *Int
 	max    *Int
+	fmax   *Int
 	reader io.Reader
 }
 
 // Initialize a new Gen with min and max values
 func NewGen(min, max *Int) Gen {
-	gen := Gen{min, max, rand.Reader}
+	fmax := NewInt(0)
+	gen := Gen{min, max, fmax.Sub(max, min), rand.Reader}
 	return gen
 }
 
-// Generates a random Int between min and max, performance degrades
-// as min approaches max
+// Generates a random Int between min and max
 func (gen Gen) Rand(x *Int) *Int {
 	ran, err := rand.Int(gen.reader, gen.max.value)
 	if err != nil {
 		return nil
 	}
 	x.value = ran
-
-	for x.Cmp(gen.min) < 0 {
-		ran, err := rand.Int(gen.reader, gen.max.value)
-		if err != nil {
-			return nil
-		}
-		x.value = ran
-	}
+	x = x.Add(x, gen.min)
 	return x
 }
