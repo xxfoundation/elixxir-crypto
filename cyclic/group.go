0ea66216fc964e53787925e75c0545b3e9c15d83
William Carter <willcarter@Williams-MacBook-Pro.local>
2017-12-19 17:40:02 -0800

    added NewGroup, Mul, Inside, and Inverse functions and tests to group.go

--- cyclic/group.go
+++ cyclic/group.go
@@ -9,59 +9,34 @@ import (
 // Groups provide cyclic int operations that keep the return values confined to
 // a finite field under modulo p
 type Group struct {
-	prime Int
-	seed  Int
+	prime *Int
+	seed  *Int
 	g     Gen
 }
 
 // NewGroup returns a group with the given prime, seed, and generator
-func NewGroup(p *Int, gen Gen) *Group {
-	err := errors.New("Unimplemented function: Group.NewGroup recieved " +
-		reflect.TypeOf(p).String() + " " + reflect.TypeOf(gen).String() + "\n")
-
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	return nil
+func NewGroup(p *Int, s *Int, g Gen) Group {
+	return Group{p, s, g}
 }
 
 // Mul multiplies a and b within the group, putting the result in c
 // and returning c
 func (g Group) Mul(a, b, c *Int) *Int {
-	err := errors.New("Unimplemented function: Group.Mul recieved " +
-		reflect.TypeOf(a).String() + " " + reflect.TypeOf(b).String() +
-		reflect.TypeOf(c).String() + "\n")
-
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	return nil
+	return c.Mod((c.Mul(a, b)), g.prime)
 }
 
 // Inside returns true of the Int is within the group, false if it isn't
 func (g Group) Inside(a *Int) bool {
-	err := errors.New("Unimplemented function: Group.Inside recieved " +
-		reflect.TypeOf(a).String() + "\n")
-
-	if err != nil {
-		fmt.Print(err)
+	if a.Cmp(NewInt(0)) != 1 || a.Cmp(g.prime) != -1 {
+		return false
+	} else {
+		return true
 	}
-
-	return false
 }
 
 // Inverse sets b equal to the inverse of a within the group and returns b
 func (g Group) Inverse(a, b *Int) *Int {
-	err := errors.New("Unimplemented function: Group.Inverse recieved " +
-		reflect.TypeOf(a).String() + " " + reflect.TypeOf(b).String() + "\n")
-
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	return nil
+	return b.ModInverse(a, g.prime)
 }
 
 // SetSeed sets a seed for use in random number generation
--- cyclic/group_test.go
+++ cyclic/group_test.go
@@ -1,65 +1,128 @@
 package cyclic
 
-/*
 import (
 	"testing"
 )
 
-
+// Tests NewGroup functionality
 func TestNewGroup(t *testing.T) {
-	expected := nilGroup()
-	p := NewInt(42)
-	g := Gen{blah: *NewInt(0)}
+	p := NewInt(1000000010101111111)
+	s := NewInt(192395897203)
+	min := NewInt(2)
+	max := NewInt(0)
+	max.Mul(p, NewInt(1000))
+	g := NewGen(min, max)
+	actual := NewGroup(p, s, g)
 
-	actual := NewGroup(p, g)
-
-	if actual != expected {
-		t.Errorf("TestNewGroup failed, expected: '%v', got: '%v'",
-			expected, actual)
+	type testStruct struct {
+		prime *Int
+		seed  *Int
+		g     Gen
+	}
+	expected := testStruct{p, s, g}
+	tests := 1
+	pass := 0
+	if actual.prime.Cmp(expected.prime) != 0 {
+		t.Errorf("TestNewGroup failed to initialize prime, expected: '%v',"+
+			" got: '%v'", expected.prime.Text(10), actual.prime.Text(10))
+	} else if actual.seed.Cmp(expected.seed) != 0 {
+		t.Errorf("TestNewGroup failed to initialize seed, expected: '%v',"+
+			" got: '%v'", expected.seed.Text(10), actual.seed.Text(10))
+	} else {
+		pass++
+		println("NewGroup()", pass, "out of", tests, "tests passed.")
 	}
 }
 
 func TestMulForGroup(t *testing.T) {
-	expected := nilInt()
-	gen := Gen{blah: *NewInt(0)}
-	g := Group{prime: *NewInt(0), seed: *NewInt(0), g: gen}
-	a := NewInt(42)
-	b := NewInt(42)
-	c := NewInt(42)
-	actual := g.Mul(a, b, c)
+	p := NewInt(17)
+	s := NewInt(15)
+	min := NewInt(2)
+	max := NewInt(1000)
+	gen := NewGen(min, max)
+	group := NewGroup(p, s, gen)
 
-	if actual != expected {
-		t.Errorf("TestMulForGroup failed, expected: '%v', got: '%v'",
-			expected, actual)
+	actual := []*Int{
+		group.Mul(NewInt(20), NewInt(10), NewInt(0)),
+		group.Mul(NewInt(0), NewInt(10), NewInt(0)),
 	}
+	expected := []*Int{
+		NewInt((20 * 10) % 17),
+		NewInt(0),
+	}
+
+	tests := len(actual)
+	pass := 0
+	for i := 0; i < len(actual); i++ {
+		if actual[i].Cmp(expected[i]) != 0 {
+			t.Errorf("TestMulForGroup failed at index:%v, expected:%v, got:%v",
+				i, expected[i].Text(10), actual[i].Text(10))
+		} else {
+			pass++
+		}
+	}
+	println("Mul()", pass, "out of", tests, "tests passed.")
 }
 
 func TestInside(t *testing.T) {
-	expected := false
-	gen := Gen{blah: *NewInt(0)}
-	g := Group{prime: *NewInt(0), seed: *NewInt(0), g: gen}
-	actual := g.Inside(NewInt(0))
-
-	if actual != expected {
-		t.Errorf("TestInside failed, expected: '%v', got: '%v'",
-			expected, actual)
+	p := NewInt(17)
+	s := NewInt(15)
+	min := NewInt(2)
+	max := NewInt(1000)
+	gen := NewGen(min, max)
+	group := NewGroup(p, s, gen)
+	expected := []bool{
+		false,
+		false,
+		false,
+		true,
 	}
+	actual := []bool{
+		group.Inside(NewInt(0)),
+		group.Inside(NewInt(17)),
+		group.Inside(NewInt(18)),
+		group.Inside(NewInt(12)),
+	}
+	tests := len(expected)
+	pass := 0
+	for i := 0; i < len(expected); i++ {
+		if actual[i] != expected[i] {
+			t.Errorf("TestInside failed at index:%v, expected:%v, got:%v",
+				i, expected, actual)
+		} else {
+			pass++
+		}
+	}
+	println("Inside()", pass, "out of", tests, "tests passed.")
 }
 
 func TestInverse(t *testing.T) {
-	expected := nilInt()
-	gen := Gen{blah: *NewInt(0)}
-	g := Group{prime: *NewInt(0), seed: *NewInt(0), g: gen}
-	a := NewInt(42)
-	b := NewInt(42)
-	actual := g.Inverse(a, b)
+	p := NewInt(17)
+	s := NewInt(15)
+	min := NewInt(2)
+	max := NewInt(1000)
+	gen := NewGen(min, max)
+	group := NewGroup(p, s, gen)
+	x := NewInt(13)
+	a := NewInt(10)
+	inv := NewInt(0)
+	inv = group.Inverse(a, inv)
+	a = group.Mul(x, a, a)
+	c := group.Mul(inv, a, NewInt(0))
 
-	if actual != expected {
+	tests := 1
+	pass := 0
+
+	if c.Cmp(x) != 0 {
 		t.Errorf("TestInverse failed, expected: '%v', got: '%v'",
-			expected, actual)
+			x.Text(10), c.Text(10))
+	} else {
+		pass++
 	}
+	println("Inverse()", pass, "out of", tests, "tests passed.")
 }
 
+/*
 func TestSetSeed(t *testing.T) {
 	expected := NewInt(0)
 	gen := Gen{blah: *NewInt(0)}
