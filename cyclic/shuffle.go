614307801b2cd3e1d710c6cb8d8a63f27f2c9f7b
Benjamin Wenger <ben@privategrity.com>
2018-01-08 23:00:43 +0000

    slc -> slice

--- cyclic/shuffle.go
+++ cyclic/shuffle.go
@@ -1,19 +1,19 @@
 package cyclic
 
 // Shuffles a passed interface array using a Fisher-Yates shuffle
-func Shuffle(slc *[]uint64) {
+func Shuffle(slice *[]uint64) {
 
-	g := NewGen(NewInt(0), NewInt(facelen(slc)-1))
+	g := NewGen(NewInt(0), NewInt(facelen(slice)-1))
 
 	x := NewInt(1<<63 - 1)
 
-	for i := int64(0); i < facelen(slc); i++ {
+	for i := int64(0); i < facelen(slice); i++ {
 		j := g.Rand(x).Int64()
-		(*slc)[j], (*slc)[i] = (*slc)[i], (*slc)[j]
+		(*slice)[j], (*slice)[i] = (*slice)[i], (*slice)[j]
 	}
 
 }
 
-func facelen(slc *[]uint64) int64 {
-	return int64(len(*slc))
+func facelen(slice *[]uint64) int64 {
+	return int64(len(*slice))
 }
