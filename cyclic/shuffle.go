fc6bf0001d9823d5653e9d187126bc768d24c314
Benjamin Wenger <ben@privategrity.com>
2018-01-08 23:05:05 +0000

    removed facelen

--- cyclic/shuffle.go
+++ cyclic/shuffle.go
@@ -3,17 +3,13 @@ package cyclic
 // Shuffles a passed interface array using a Fisher-Yates shuffle
 func Shuffle(slice *[]uint64) {
 
-	g := NewGen(NewInt(0), NewInt(facelen(slice)-1))
+	g := NewGen(NewInt(0), NewInt(int64(len(*slice))-1))
 
 	x := NewInt(1<<63 - 1)
 
-	for i := int64(0); i < facelen(slice); i++ {
+	for i := int64(0); i < int64(len(*slice)); i++ {
 		j := g.Rand(x).Int64()
 		(*slice)[j], (*slice)[i] = (*slice)[i], (*slice)[j]
 	}
 
-}
-
-func facelen(slice *[]uint64) int64 {
-	return int64(len(*slice))
 }
