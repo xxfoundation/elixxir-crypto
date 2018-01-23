1e8d48b8bacb818978665986f655af8eca0f2596
Spencer Brown <spencer@privategrity.com>
2018-01-22 16:37:39 -0800

    Update shared key generation to use correct inputs and outputs

--- cryptops/keygen.go
+++ cryptops/keygen.go
@@ -5,26 +5,41 @@ import (
 	"gitlab.com/privategrity/crypto/hash"
 )
 
-func GenerateKey(toHash *cyclic.Int, byteLength int) *cyclic.Int {
+// Generates a new shared key
+// out is an optional parameter that allows you to pre-allocate a big integer
+// TODO: ensure that we put out the correct key length (close to prime length)
+// to make the distribution as uniform as possible
+func GenerateSharedKey(byteLength int, g *cyclic.Group, baseKey,
+	prevRecursiveKey, outRecursiveKey, outSharedKey *cyclic.Int) *cyclic.Int {
 	// TODO: don't construct new hash every time
-	h, err := hash.NewCMixHash()
+	hash, _ := hash.NewCMixHash()
 
-	var result []byte
+	sharedKey := baseKey.Bytes()
 
 	// F(x)
-	h.Write(toHash.Bytes())
-	result = h.Sum(result)
+	hash.Write(sharedKey)
+	// hash.Sum behaves similarly to append() for slices
+	sharedKey = hash.Sum(nil)
 
 	// G(x)
-	for len(result) < byteLength {
-		h.Write(result)
-		result = h.Sum(result)
-		println(h.Size())
+	hash.Write(prevRecursiveKey.Bytes())
+	sharedKey = hash.Sum(sharedKey)
+	for len(sharedKey) < byteLength {
+		// each iteration of G adds 32 to the length of the shared key
+		hash.Write(sharedKey)
+		sharedKey = hash.Sum(sharedKey)
 	}
 
-	// H(x)
-	h.Write(result)
-	result = h.Sum(result)
+	// limit the length of the recursive key output
+	one := cyclic.NewInt(1)
+	g.Mul(one, cyclic.NewIntFromBytes(sharedKey), outRecursiveKey)
 
-	return cyclic.NewIntFromBytes(result[:byteLength])
+	// H(x)
+	hash.Write(sharedKey)
+	sharedKey = hash.Sum(sharedKey)
+
+	// limit the length of the shared key output
+	g.Mul(one, cyclic.NewIntFromBytes(sharedKey), outSharedKey)
+
+	return outSharedKey
 }
--- cryptops/keygen_test.go
+++ cryptops/keygen_test.go
@@ -1,7 +1,18 @@
 package cryptops
 
-import "testing"
+import (
+	"gitlab.com/privategrity/crypto/cyclic"
+	"testing"
+)
 
-func TestGenerateKey(t *testing.T) {
-	t.Error("Test not implemented")
+func TestGenerateSharedKey(t *testing.T) {
+	g := cyclic.NewGroup(cyclic.NewInt(7919), cyclic.NewInt(55), cyclic.NewInt(33), cyclic.NewRandom(cyclic.NewInt(0), cyclic.NewInt(1000)))
+
+	outRecursiveKey := cyclic.NewMaxInt()
+	outSharedKey := cyclic.NewMaxInt()
+
+	GenerateSharedKey(1024, &g, cyclic.NewInt(78), cyclic.NewInt(90), outRecursiveKey, outSharedKey)
+	println(outRecursiveKey.Text(16), outSharedKey.Text(16))
+
+	t.Error("Test implementation not finished")
 }
