82aafe90dd507832ecc4c90cddd769ebae04195f
Spencer Brown <spencer@privategrity.com>
2018-01-23 13:50:03 -0800

    Many improvements to shared key generation
     - Reset hash for each phase
     - Combine recursive key and base key in a way that's more compatible
       with various hash functions
     - Better shared key length limiting (probably)
     - More robust runtime error checking for input key length

--- cryptops/keygen.go
+++ cryptops/keygen.go
@@ -5,41 +5,104 @@ import (
 	"gitlab.com/privategrity/crypto/hash"
 )
 
+// Combine two keys without losing entropy
+func bitwiseXOR(a []byte, b []byte, out []byte) []byte {
+	for i := 0; i < len(a); i++ {
+		out[i] = a[i] ^ b[i]
+	}
+
+	return out
+}
+
+// Limit key bit length to a multiple of the prime's bit length
+func limitKeyLength(sharedKey []byte, prime *cyclic.Int) []byte {
+	sharedKeyLength := cyclic.NewIntFromBytes(sharedKey).BitLen()
+	println(sharedKeyLength)
+	bitsToZero := sharedKeyLength % prime.BitLen()
+	println("Bits to zero:")
+	println(bitsToZero)
+	bitsZeroed := 0
+
+	// zero most of the bits that we need to zero
+	i := 0
+	for i < bitsToZero/8 {
+		sharedKey[i] = 0
+		// debug
+		sharedKeyLength = cyclic.NewIntFromBytes(sharedKey).BitLen()
+		println("Shared key statistics:")
+		println(sharedKeyLength)
+		println(sharedKeyLength % prime.BitLen())
+		println(sharedKeyLength / prime.BitLen())
+		// end debug
+		i++
+		bitsZeroed += 8
+	}
+
+	// zero the last few bits
+	bitMask := byte(1 << 7)
+	for bitsZeroed < bitsToZero {
+		bitsZeroed++
+		sharedKey[i] = sharedKey[i] &^ bitMask
+		// debug
+		sharedKeyLength = cyclic.NewIntFromBytes(sharedKey).BitLen()
+		println("Shared key statistics:")
+		println(sharedKeyLength)
+		println(sharedKeyLength % prime.BitLen())
+		println(sharedKeyLength / prime.BitLen())
+		// end debug
+		bitMask >>= 1
+	}
+
+	return sharedKey
+}
+
 // Generates a new shared key
-// out is an optional parameter that allows you to pre-allocate a big integer
-// TODO: ensure that we put out the correct key length (close to prime length)
-// to make the distribution as uniform as possible
+// Calling this method will mutate recursiveKey
+// TODO: is it better to take bitLength instead of byteLength?
+// it might make the function more complicated
 func GenerateSharedKey(byteLength int, g *cyclic.Group, baseKey,
-	prevRecursiveKey, outRecursiveKey, outSharedKey *cyclic.Int) *cyclic.Int {
+	recursiveKey, outSharedKey *cyclic.Int) *cyclic.Int {
+	if baseKey.BitLen() != 256 {
+		panic("Base key has non-256 bit length")
+	}
+	if recursiveKey.BitLen() != 256 {
+		panic("Recursive key has non-256 bit length")
+	}
+
 	// TODO: don't construct new hash every time
 	hash, _ := hash.NewCMixHash()
-
-	sharedKey := baseKey.Bytes()
+	temp := cyclic.NewInt(1)
 
 	// F(x)
-	hash.Write(sharedKey)
-	// hash.Sum behaves similarly to append() for slices
-	sharedKey = hash.Sum(nil)
+	hash.Reset()
+	hash.Write(baseKey.Bytes())
+	fOut := hash.Sum(nil)
 
 	// G(x)
-	hash.Write(prevRecursiveKey.Bytes())
-	sharedKey = hash.Sum(sharedKey)
+	hash.Reset()
+	fOut = bitwiseXOR(fOut, recursiveKey.Bytes(), fOut)
+	hash.Write(fOut)
+	recursiveKey.SetBytes(hash.Sum(nil))
+
+	// H(x)
+	hash.Reset()
+	sharedKey := make([]byte, 0, byteLength)
+	hashInput := cyclic.NewIntFromBytes(recursiveKey.Bytes())
+
+	// each iteration of H adds 256 bits to the length of the shared key
 	for len(sharedKey) < byteLength {
-		// each iteration of G adds 32 to the length of the shared key
-		hash.Write(sharedKey)
+		// increment hash input
+		hashInput = hashInput.Add(temp, hashInput)
+		hash.Reset()
+		hash.Write(hashInput.Bytes())
 		sharedKey = hash.Sum(sharedKey)
 	}
 
-	// limit the length of the recursive key output
-	one := cyclic.NewInt(1)
-	g.Mul(one, cyclic.NewIntFromBytes(sharedKey), outRecursiveKey)
-
-	// H(x)
-	hash.Write(sharedKey)
-	sharedKey = hash.Sum(sharedKey)
-
-	// limit the length of the shared key output
-	g.Mul(one, cyclic.NewIntFromBytes(sharedKey), outSharedKey)
+	// limit the length of the shared key output with a modulus
+	g.GetP(temp)
+	sharedKey = limitKeyLength(sharedKey, temp)
+	outSharedKey.SetBytes(sharedKey)
+	outSharedKey.Mod(temp, outSharedKey)
 
 	return outSharedKey
 }
--- cryptops/keygen_test.go
+++ cryptops/keygen_test.go
@@ -6,13 +6,23 @@ import (
 )
 
 func TestGenerateSharedKey(t *testing.T) {
-	g := cyclic.NewGroup(cyclic.NewInt(7919), cyclic.NewInt(55), cyclic.NewInt(33), cyclic.NewRandom(cyclic.NewInt(0), cyclic.NewInt(1000)))
+	g := cyclic.NewGroup(cyclic.NewIntFromString("68636564122675662743823714992884378001308422399791648446212449933215410614414642667938213644208420192054999687", 10), cyclic.NewInt(55), cyclic.NewInt(33), cyclic.NewRandom(cyclic.NewInt(0), cyclic.NewInt(1000)))
 
-	outRecursiveKey := cyclic.NewMaxInt()
+	secretSeedKey := cyclic.NewIntFromString(
+		"ef9ab83927cd2349f98b1237889909002b897231ae9c927d1792ea0879287ea3", 16)
+
+	println("ssk bitlen:", secretSeedKey.BitLen())
 	outSharedKey := cyclic.NewMaxInt()
 
-	GenerateSharedKey(1024, &g, cyclic.NewInt(78), cyclic.NewInt(90), outRecursiveKey, outSharedKey)
-	println(outRecursiveKey.Text(16), outSharedKey.Text(16))
+	baseKey := cyclic.NewIntFromString(
+		"da9f8137821987b978164932015c105263ae769310269b510937c190768e2930", 16)
+
+	println("bk bitlen:", baseKey.BitLen())
+
+	// Shared key is generated from an 8192-bit key
+	println("Before:", secretSeedKey.Text(16), outSharedKey.Text(16))
+	GenerateSharedKey(8192, &g, baseKey, secretSeedKey, outSharedKey)
+	println("After:", secretSeedKey.Text(16), outSharedKey.Text(16))
 
 	t.Error("Test implementation not finished")
 }
