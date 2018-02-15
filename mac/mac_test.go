74ff2ec0cfd060466c3a6af5bc891b36ba073ebf
Richard T. Carback III <rick.carback@gmail.com>
2018-02-15 22:11:03 +0000

    MAC Test

---
+++ mac/mac_test.go
@@ -0,0 +1,29 @@
+package mac
+
+import(
+	"testing"
+)
+
+func TestMAC(t *testing.T) {
+	message := []byte("Blah")
+	key := []byte("Blah2")
+	mac := MessageAuthenticationCode(message, key)
+	if !VerifyMessageAuthenticationCode(message, key, mac) {
+		t.Errorf("MAC is not working!")
+	}
+	expected := []byte{
+		136, 145, 4, 147, 50, 117, 134, 168, 154, 75, 79, 176, 224, 26,
+		122, 50, 80, 8, 57, 4, 60, 14, 178, 33, 8, 1, 111, 91, 50, 188,
+		119, 42,
+	}
+	if len(expected) != len(mac) {
+		t.Errorf("MAC has changed length: %d expected, got %d", len(expected),
+			len(mac))
+	}
+	for i := 0; i < len(expected); i++ {
+		if expected[i] != mac[i] {
+			t.Errorf("MAC Byte Differs at %d: %d expected, got %d", i, expected[i],
+				mac[i])
+		}
+	}
+}
