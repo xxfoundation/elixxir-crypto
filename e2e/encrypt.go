6ee6c372b134be2472fe1dec4475b397e423b7d7
Jono <jono@privategrity.com>
2019-02-27 11:12:36 -0800

    Implement Decrypt() function.

--- e2e/encrypt.go
+++ e2e/encrypt.go
@@ -23,3 +23,20 @@ func Encrypt(g cyclic.Group, key, msg cyclic.Int) (cyclic.Int, error) {
 	// Return the result
 	return *product, nil
 }
+
+// Modular inverts the key under the passed group and modular multiplies it with
+// the encrypted message under the passed group.
+func Decrypt(g cyclic.Group, key, encMsg cyclic.Int) cyclic.Int {
+	keyInv := cyclic.NewInt(0)
+
+	// Modular invert the key under the group
+	keyInv = g.Inverse(&key, keyInv)
+
+	product := cyclic.NewInt(0)
+
+	// Modular multiply the inverted key with the message
+	product = g.Mul(keyInv, &encMsg, product)
+
+	// Return the result
+	return *product
+}
