a35bd7d1f52469b20cbd2461cdc1256f02735fae
Tigran Avakyan <tigran.avakyan@icloud.com>
2019-02-27 17:31:17 -0800

    Refactoring according to new error handling standard. Need to get full coverage on unpad function

--- e2e/padding.go
+++ e2e/padding.go
@@ -1,7 +1,15 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
 package e2e
 
 import (
+	"bytes"
 	"crypto/rand"
+	"errors"
 	jww "github.com/spf13/jwalterweatherman"
 	"io"
 )
@@ -11,18 +19,23 @@ const MinPaddingStringLen= 8
 const NumFixedPaddingLen = 3
 const MinPaddingLen = MinPaddingStringLen + NumFixedPaddingLen
 
+var ErrMessageTooLong = errors.New("message too long")
+var ErrEncMessageLength = errors.New("encoded message less than min. padding length")
+var ErrPaddingPrefix = errors.New("padding prefix invalid")
+var ErrPaddingContainsZero = errors.New("padding string contains a zero")
+var ErrPaddingTerminator = errors.New("padding terminator invalid")
 
-func Pad(msg []byte, encMsgLen int) (encMsg []byte) {
+// PKCS 1.5 Pad using crypto.rand.Reader
+func Pad(msg []byte, encMsgLen int) (encMsg []byte, err error) {
 	return pad(msg, encMsgLen, rand.Reader)
 }
 
-
 // PKCS 1.5 Pad (See RFC 3447 7.2.1 https://tools.ietf.org/html/rfc3447#section-7.2.1)
-func pad(msg []byte, encMsgLen int, rand io.Reader) (encMsg []byte) {
+func pad(msg []byte, encMsgLen int, rand io.Reader) (encMsg []byte, err error) {
 	msgLen := len(msg)
 
 	if msgLen > encMsgLen - MinPaddingLen {
-		jww.FATAL.Panicf("Could not add padding: message too long")
+		return nil, ErrMessageTooLong
 	}
 
 	// RFC 3447 7.2.1-2 defines the following padding format:
@@ -45,35 +58,30 @@ func pad(msg []byte, encMsgLen int, rand io.Reader) (encMsg []byte) {
 	msgBuffer := encMsg[encMsgLen - msgLen:]
 	copy(msgBuffer, msg)
 
-	return encMsg
+	return encMsg, nil
 }
 
 // PKCS 1.5 Unpad (See RFC 3447 7.2.1 https://tools.ietf.org/html/rfc3447#section-7.2.1)
-func Unpad(encMsg []byte) (msg []byte) {
+func Unpad(encMsg []byte) (msg []byte, err error) {
 	encMsgLen := len(encMsg)
 
 	if encMsgLen < MinPaddingLen {
-		jww.FATAL.Panicf("Could not remove padding: encoded message is less than min padding length")
+		return nil, ErrEncMessageLength
 	}
 
 	// RFC 3447 7.2.1-2 defines the following padding format:
 	// encMsg = 0x00 || 0x02 || paddingString || 0x00 || message
 
-	// Check 1st padding octet
-	if encMsg[0] != 0x00 {
-		jww.FATAL.Panicf("Could not remove padding: padding prefix invalid because 1st octet of encoded message must be 0x00")
-	}
-
-	// Check 2nd padding octet
-	if encMsg[1] != 0x02 {
-		jww.FATAL.Panicf("Could not remove padding: padding prefix invalid because 2nd octet of encoded message must be 0x02")
+	// Check padding prefix
+	if !bytes.Equal(encMsg[0:2], []byte{0x00, 0x02}) {
+		return nil, ErrPaddingPrefix
 	}
 
 	// Check that the smallest possible padding string contains non-zero octets.
 	minPaddingStr := encMsg[2:MinPaddingLen-1]
 	for _, oct := range minPaddingStr {
 		if oct == 0x00 {
-			jww.FATAL.Panicf("Could not remove padding: min. sized padding string contains an octet with a value of zero")
+			return nil, ErrPaddingContainsZero
 		}
 	}
 
@@ -81,15 +89,14 @@ func Unpad(encMsg []byte) (msg []byte) {
 	termInd := 0
 	for i, oct := range encMsg[MinPaddingLen:] {
 		if oct == 0x00 {
-			jww.INFO.Printf("Found end of padding in encoded message at %v", termInd)
+			jww.INFO.Printf("found end of padding in encoded message at %v", termInd)
 			termInd = i
 			break
 		}
 	}
 	if termInd == 0 {
-		jww.FATAL.Panicf("Could not remove padding: unable to detect terminating octet index")
+		return nil, ErrPaddingTerminator
 	}
-
 
 	// Extract message after padding
 	msg = encMsg[termInd+1:]
@@ -97,21 +104,20 @@ func Unpad(encMsg []byte) (msg []byte) {
 
 	// Check that message is not too long
 	if msgLen > encMsgLen - MinPaddingLen {
-		jww.FATAL.Panicf("Could not remove padding: message too long")
+		return nil, ErrMessageTooLong
 	}
 
-	return msg
-
+	return msg, nil
 }
 
-// nonZeroRandomBytes fills the given slice with non-zero random octets.
-// Function nonZeroRandomBytes taken from x/crypto/openpgp/elgamal/elgamal.go and crypto/rsa/pkcs1v15.go
-// and modified to explicitly panic instead of return an error.
+// Fills the given slice with non-zero random octets.
+// Taken from x/crypto/openpgp/elgamal/elgamal.go and crypto/rsa/pkcs1v15.go
+// and modified to explicitly panic on external Reader failure instead of returning an error
 func nonZeroRandomBytes(s []byte, rand io.Reader) {
 	// Fill slice with random octets (including zeros)
 	_, err := io.ReadFull(rand, s)
 	if err != nil {
-		jww.FATAL.Panicf("Error readding full length of buffer from rand Reader into slice: %v", err.Error())
+		jww.FATAL.Panicf("error reading full length of buffer from rand Reader into slice: %v", err.Error())
 	}
 
 	for i := 0; i < len(s); i++ {
@@ -119,7 +125,7 @@ func nonZeroRandomBytes(s []byte, rand io.Reader) {
 		for s[i] == 0 {
 			_, err := io.ReadFull(rand, s[i:i+1])
 			if err != nil {
-				jww.FATAL.Panicf("Could not repolace zero octet with a non-zero octet: %v", err.Error())
+				jww.FATAL.Panicf("could not repolace zero octet with a non-zero octet: %v", err.Error())
 			}
 		}
 	}
