309e2ae3c60a5239604caa1243c46877e10f9b6c
Tigran Avakyan <tigran.avakyan@icloud.com>
2019-03-08 14:00:44 -0800

    Addding most tests

--- signature/dsa_test.go
+++ signature/dsa_test.go
@@ -7,10 +7,9 @@
 package signature
 
 import (
-	"crypto/dsa"
+	cryptoRand "crypto/rand"
 	"errors"
 	"gitlab.com/elixxir/crypto/cyclic"
-	"io"
 	"math/big"
 	"math/rand"
 	"testing"
@@ -19,6 +18,10 @@ import (
 func TestCustomDSAParams(t *testing.T) {
 
 	var pVal, qVal, gVal int64 = 0, 0, 0
+
+	p := cyclic.NewInt(pVal)
+	q := cyclic.NewInt(qVal)
+	g := cyclic.NewInt(gVal)
 
 	dsaParams := CustomDSAParams(p, q, g)
 
@@ -47,10 +50,6 @@ func TestCustomDSAParams(t *testing.T) {
 	}
 
 	pVal, qVal, gVal = 123, 456, 789
-
-	p := cyclic.NewInt(pVal)
-	q := cyclic.NewInt(qVal)
-	g := cyclic.NewInt(gVal)
 
 	dsaParams = CustomDSAParams(p, q, g)
 
@@ -81,7 +80,7 @@ func TestNewDSAParamsPanic(t *testing.T) {
 	}()
 
 	r := AlwaysErrorReader{}
-	NewDSAParams(r, L1024N160)
+	NewDSAParams(&r, L1024N160)
 
 }
 
@@ -164,174 +163,61 @@ func TestDSAPublicKeyGen(t *testing.T) {
 
 	privateKey.GetKey()
 
-	y := privateKey.PublicKeyGen().key.Y
+	// y := privateKey.PublicKeyGen().key.Y
 
 	// check if y is valid!
 
 }
 
-
 func fromHex(s string) *cyclic.Int {
-
 	result, ok := new(big.Int).SetString(s, 16)
 
 	if !ok {
-
 		panic(s)
-
 	}
+
 	return cyclic.NewIntFromBigInt(result)
-
 }
-
 
 func TestSignAndVerify(t *testing.T) {
 
+	p := fromHex("A9B5B793FB4785793D246BAE77E8FF63CA52F442DA763C440259919FE1BC1D6065A9350637A04F75A2F039401D49F08E066C4D275A5A65DA5684BC563C14289D7AB8A67163BFBF79D85972619AD2CFF55AB0EE77A9002B0EF96293BDD0F42685EBB2C66C327079F6C98000FBCB79AACDE1BC6F9D5C7B1A97E3D9D54ED7951FEF")
+	q := fromHex("E1D3391245933D68A0714ED34BBCB7A1F422B9C1")
+	g := fromHex("634364FC25248933D01D1993ECABD0657CC0CB2CEED7ED2E3E8AECDFCDC4A25C3B15E9E3B163ACA2984B5539181F3EFF1A5E8903D71D5B95DA4F27202B77D2C44B430BB53741A8D59A8F86887525C9F2A6A5980A195EAA7F2FF910064301DEF89D3AA213E1FAC7768D89365318E370AF54A112EFBA9246D9158386BA1B4EEFDA")
 
-	p, q, g = *cyclic.NewInt(1), *cyclic.NewInt(2), *cyclic.NewInt(3)
+	params := CustomDSAParams(p, q, g)
 
-	cyclic.Fr
+	y := fromHex("32969E5780CFE1C849A1C276D7AEB4F38A23B591739AA2FE197349AEEBD31366AEE5EB7E6C6DDB7C57D02432B30DB5AA66D9884299FAA72568944E4EEDC92EA3FBC6F39F53412FBCC563208F7C15B737AC8910DBC2D9C9B8C001E72FDC40EB694AB1F06A5A2DBD18D9E36C66F31F566742F11EC0A52E9F7B89355C02FB5D32D2")
 
-	priv := dsa.PrivateKey{
+	ReconstructPublicKey(params, y) // publicKey := ReconstructPublicKey(params, y)
 
-		PublicKey: dsa.PublicKey{
-
-			Parameters: dsa.Parameters{
-
-				P: fromHex("A9B5B793FB4785793D246BAE77E8FF63CA52F442DA763C440259919FE1BC1D6065A9350637A04F75A2F039401D49F08E066C4D275A5A65DA5684BC563C14289D7AB8A67163BFBF79D85972619AD2CFF55AB0EE77A9002B0EF96293BDD0F42685EBB2C66C327079F6C98000FBCB79AACDE1BC6F9D5C7B1A97E3D9D54ED7951FEF"),
-
-				Q: fromHex("E1D3391245933D68A0714ED34BBCB7A1F422B9C1"),
-
-				G: fromHex("634364FC25248933D01D1993ECABD0657CC0CB2CEED7ED2E3E8AECDFCDC4A25C3B15E9E3B163ACA2984B5539181F3EFF1A5E8903D71D5B95DA4F27202B77D2C44B430BB53741A8D59A8F86887525C9F2A6A5980A195EAA7F2FF910064301DEF89D3AA213E1FAC7768D89365318E370AF54A112EFBA9246D9158386BA1B4EEFDA"),
-
-			},
-
-			Y: fromHex("32969E5780CFE1C849A1C276D7AEB4F38A23B591739AA2FE197349AEEBD31366AEE5EB7E6C6DDB7C57D02432B30DB5AA66D9884299FAA72568944E4EEDC92EA3FBC6F39F53412FBCC563208F7C15B737AC8910DBC2D9C9B8C001E72FDC40EB694AB1F06A5A2DBD18D9E36C66F31F566742F11EC0A52E9F7B89355C02FB5D32D2"),
-
-		},
-
-		X: fromHex("5078D4D29795CBE76D3AACFE48C9AF0BCDBEE91A"),
-
-	}
-
-
-	testSignAndVerify(t, 0, &priv)
+	//x := X: fromHex("5078D4D29795CBE76D3AACFE48C9AF0BCDBEE91A")
+	//
+	//privateKey := ReconstructPrivateKey(publicKey, x)
+	//
+	//testSignAndVerify(t, 0, &priv)
 
 }
 
-
-func TestSignAndVerify(t *testing.T, i int, priv *PrivateKey) {
+func testSignAndVerify(t *testing.T, i int, priv *DSAPrivateKey) {
 
 	hashed := []byte("testing")
 
-	p, q, g = *cyclic.NewInt(1), *cyclic.NewInt(2), *cyclic.NewInt(3)
-	params := CustomDSAParams(p, q, g)
-	params.PrivateKeyGen(rng)
-
-	privateKey := PrivateKeyGen(rng io.Reader)
-
-	r, s, err := Sign(rand.Reader, priv, hashed)
+	signature, err := priv.Sign(hashed, cryptoRand.Reader)
 
 	if err != nil {
 		t.Errorf("%d: error signing: %s", i, err)
 		return
 	}
 
-	if !Verify(&priv.PublicKey, hashed, r, s) {
+	publicKey := priv.PublicKeyGen()
+
+	if !publicKey.Verify(hashed, *signature) {
+
 		t.Errorf("%d: Verify failed", i)
 	}
 
 }
-
-func testParameterGeneration(t *testing.T, sizes ParameterSizes, L, N int) {
-
-	var priv PrivateKey
-
-	params := &priv.Parameters
-
-
-	err := GenerateParameters(params, rand.Reader, sizes)
-
-	if err != nil {
-
-		t.Errorf("%d: %s", int(sizes), err)
-
-		return
-
-	}
-
-	if params.P.BitLen() != L {
-
-		t.Errorf("%d: params.BitLen got:%d want:%d", int(sizes), params.P.BitLen(), L)
-
-	}
-
-
-	if params.Q.BitLen() != N {
-
-		t.Errorf("%d: q.BitLen got:%d want:%d", int(sizes), params.Q.BitLen(), L)
-
-	}
-
-	one := new(big.Int)
-
-	one.SetInt64(1)
-
-	pm1 := new(big.Int).Sub(params.P, one)
-
-	quo, rem := new(big.Int).DivMod(pm1, params.Q, new(big.Int))
-
-	if rem.Sign() != 0 {
-
-		t.Errorf("%d: p-1 mod q != 0", int(sizes))
-
-	}
-
-	x := new(big.Int).Exp(params.G, quo, params.P)
-
-	if x.Cmp(one) == 0 {
-
-		t.Errorf("%d: invalid generator", int(sizes))
-
-	}
-
-
-	err = GenerateKey(&priv, rand.Reader)
-
-	if err != nil {
-
-		t.Errorf("error generating key: %s", err)
-
-		return
-
-	}
-
-
-	testSignAndVerify(t, int(sizes), &priv)
-
-}
-
-
-func TestParameterGeneration(t *testing.T) {
-
-	if testing.Short() {
-
-		t.Skip("skipping parameter generation test in short mode")
-
-	}
-
-
-	testParameterGeneration(t, L1024N160, 1024, 160)
-
-	testParameterGeneration(t, L2048N224, 2048, 224)
-
-	testParameterGeneration(t, L2048N256, 2048, 256)
-
-	testParameterGeneration(t, L3072N256, 3072, 256)
-
-}
-
-
 
 func TestSigningWithDegenerateKeys(t *testing.T) {
 
@@ -340,46 +226,32 @@ func TestSigningWithDegenerateKeys(t *testing.T) {
 	// loop.
 
 	badKeys := []struct {
-
 		p, q, g, y, x string
-
 	}{
 
 		{"00", "01", "00", "00", "00"},
 
 		{"01", "ff", "00", "00", "00"},
-
 	}
-
 
 	for i, test := range badKeys {
 
-		priv := PrivateKey{
+		const base = 10
+		p := cyclic.NewIntFromString(test.p, base)
+		q := cyclic.NewIntFromString(test.q, base)
+		g := cyclic.NewIntFromString(test.g, base)
+		y := cyclic.NewIntFromString(test.y, base)
+		x := cyclic.NewIntFromString(test.x, base)
 
-			PublicKey: PublicKey{
-
-				Parameters: Parameters{
-
-					P: fromHex(test.p),
-
-					Q: fromHex(test.q),
-
-					G: fromHex(test.g),
-
-				},
-
-				Y: fromHex(test.y),
-
-			},
-
-			X: fromHex(test.x),
-
-		}
-
+		params := CustomDSAParams(p, q, g)
+		publicKey := ReconstructPublicKey(params, y)
+		privateKey := ReconstructPrivateKey(publicKey, x)
 
 		hashed := []byte("testing")
 
-		if _, _, err := Sign(rand.Reader, &priv, hashed); err == nil {
+		_, err := privateKey.Sign(hashed, cryptoRand.Reader)
+
+		if err == nil {
 
 			t.Errorf("#%d: unexpected success", i)
 
@@ -388,3 +260,90 @@ func TestSigningWithDegenerateKeys(t *testing.T) {
 	}
 
 }
+
+//func TestParameterGeneration(t *testing.T) {
+//
+//	//if testing.Short() {
+//	//
+//	//	t.Skip("skipping parameter generation test in short mode")
+//	//
+//	//}
+//
+//
+//	testParameterGeneration(t, L1024N160, 1024, 160)
+//
+//	testParameterGeneration(t, L2048N224, 2048, 224)
+//
+//	testParameterGeneration(t, L2048N256, 2048, 256)
+//
+//	testParameterGeneration(t, L3072N256, 3072, 256)
+//
+//}
+//
+//
+//func testParameterGeneration(t *testing.T, sizes ParameterSizes, L, N int) {
+//
+//	var priv PrivateKey
+//
+//	params := &priv.Parameters
+//
+//
+//	err := GenerateParameters(params, rand.Reader, sizes)
+//
+//	if err != nil {
+//
+//		t.Errorf("%d: %s", int(sizes), err)
+//
+//		return
+//
+//	}
+//
+//	if params.P.BitLen() != L {
+//
+//		t.Errorf("%d: params.BitLen got:%d want:%d", int(sizes), params.P.BitLen(), L)
+//
+//	}
+//
+//	if params.Q.BitLen() != N {
+//
+//		t.Errorf("%d: q.BitLen got:%d want:%d", int(sizes), params.Q.BitLen(), L)
+//
+//	}
+//
+//	one := new(big.Int)
+//
+//	one.SetInt64(1)
+//
+//	pm1 := new(big.Int).Sub(params.P, one)
+//
+//	quo, rem := new(big.Int).DivMod(pm1, params.Q, new(big.Int))
+//
+//	if rem.Sign() != 0 {
+//
+//		t.Errorf("%d: p-1 mod q != 0", int(sizes))
+//
+//	}
+//
+//	x := new(big.Int).Exp(params.G, quo, params.P)
+//
+//	if x.Cmp(one) == 0 {
+//
+//		t.Errorf("%d: invalid generator", int(sizes))
+//
+//	}
+//
+//
+//	err = GenerateKey(&priv, rand.Reader)
+//
+//	if err != nil {
+//
+//		t.Errorf("error generating key: %s", err)
+//
+//		return
+//
+//	}
+//
+//
+//	testSignAndVerify(t, int(sizes), &priv)
+//
+//}
