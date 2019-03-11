4e4a1eab23d96ccb9210085b0ed58e07a8093da3
Tigran Avakyan <tigran.avakyan@icloud.com>
2019-03-11 13:49:57 -0700

    Added TestNewIntFromBigInt and ran go fmt

--- cyclic/int_test.go
+++ cyclic/int_test.go
@@ -41,30 +41,28 @@ func TestNewInt(t *testing.T) {
 
 }
 
-
 //TestNewIntFromBigInt checks if the NewIntFromBigInt function returns a cyclic Int with
 //the same value of the passed BigInt
 func TestNewIntFromBigInt(t *testing.T) {
-	
-	//tests := 1
-	//
-	//pass := 0
-	//
-	//expected := big.NewInt(int64(42))
-	//
-	//actual := NewInt(int64(42))
-	//
-	//actualData := actual.Int64()
-	//expectedData := expected.Int64()
-	//
-	//if actualData != expectedData {
-	//	t.Errorf("Test of NewInt failed, expected: '%v', got: '%v'",
-	//		actualData, expectedData)
-	//} else {
-	//	pass++
-	//}
-	//
-	//println("NewInt()", pass, "out of", tests, "tests passed.")
+	tests := 1
+
+	pass := 0
+
+	expected := big.NewInt(int64(42))
+
+	actual := NewInt(int64(42))
+
+	actualData := actual.Int64()
+	expectedData := expected.Int64()
+
+	if actualData != expectedData {
+		t.Errorf("Test of NewInt failed, expected: '%v', got: '%v'",
+			actualData, expectedData)
+	} else {
+		pass++
+	}
+
+	println("NewInt()", pass, "out of", tests, "tests passed.")
 
 }
 
