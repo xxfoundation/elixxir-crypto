8970e65e26b6c7b9154f8a1410da940c3d441ee5
Tigran Avakyan <tigran.avakyan@icloud.com>
2019-03-15 11:13:04 -0700

    Changing test to use NewUserFromUid test helper method

--- e2e/dummykeygen_test.go
+++ e2e/dummykeygen_test.go
@@ -15,10 +15,10 @@ import (
 // Test to ensure the number of keys equals the number of users to combine with
 func TestDummyKeyGen_ValidNumKeys(t *testing.T) {
 
-	currUser := generateUsers([]uint64{12345})[0]
+	currUser := generateUsers([]uint64{12345}, t)[0]
 
 	userIds := []uint64{1, 2, 3, 4, 5, 6}
-	users := generateUsers(userIds)
+	users := generateUsers(userIds, t)
 
 	keys := *KeyGen(&currUser, &users, &grp)
 	if len(keys) != len(users) {
@@ -26,14 +26,14 @@ func TestDummyKeyGen_ValidNumKeys(t *testing.T) {
 	}
 
 	userIds = []uint64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	users = generateUsers(userIds)
+	users = generateUsers(userIds, t)
 	keys = *KeyGen(&currUser, &users, &grp)
 	if len(keys) != len(users) {
 		t.Errorf("Wrong number of keys generated")
 	}
 
 	userIds = []uint64{8, 9, 10}
-	users = generateUsers(userIds)
+	users = generateUsers(userIds, t)
 	keys = *KeyGen(&currUser, &users, &grp)
 	if len(keys) != len(users) {
 		t.Errorf("Wrong number of keys generated")
@@ -44,15 +44,15 @@ func TestDummyKeyGen_ValidNumKeys(t *testing.T) {
 // is deterministic and reproducible
 func TestDummyKeyGen_KeysMatch(t *testing.T) {
 
-	currUser := generateUsers([]uint64{12345})[0]
+	currUser := generateUsers([]uint64{12345}, t)[0]
 
 	userIds := []uint64{1, 2, 3, 4, 5, 6}
-	users := generateUsers(userIds)
+	users := generateUsers(userIds, t)
 
 	keys1 := *KeyGen(&currUser, &users, &grp)
 
 	userIds = []uint64{6, 5, 4, 3, 2, 1}
-	users = generateUsers(userIds)
+	users = generateUsers(userIds, t)
 
 	keys2 := *KeyGen(&currUser, &users, &grp)
 
@@ -68,9 +68,9 @@ func TestDummyKeyGen_KeysMatch(t *testing.T) {
 // Ensure that order doesn't matter when generating a hash from two user ids
 func TestDummyKeyGen_CombinedHashCommutes(t *testing.T) {
 
-	userA := generateUsers([]uint64{12345})[0]
+	userA := generateUsers([]uint64{12345}, t)[0]
 
-	userB := generateUsers([]uint64{5})[0]
+	userB := generateUsers([]uint64{5}, t)[0]
 
 	res1 := combinedHash(&userA, &userB, &grp)
 
@@ -82,9 +82,13 @@ func TestDummyKeyGen_CombinedHashCommutes(t *testing.T) {
 }
 
 // Helper function to generate users from slice of user ids
-func generateUsers(uids []uint64) (users []id.User) {
+func generateUsers(uids []uint64, t *testing.T) (users []id.User) {
 
 	users = make([]id.User, len(uids))
+
+	for i, uid := range uids {
+		users[i] = *id.NewUserFromUint(uid, t)
+	}
 
 	var result id.User
 	for i, uid := range uids {
