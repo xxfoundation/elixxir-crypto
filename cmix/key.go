b9e9088ed87ec960d6406d081f86b1ae33a39426
Tigran Avakyan <tigran.avakyan@icloud.com>
2019-04-02 10:04:19 -0700

    Refactored messaging package to cmix

--- cmix/key.go
+++ cmix/key.go
@@ -4,7 +4,7 @@
 // All rights reserved.                                                        /
 ////////////////////////////////////////////////////////////////////////////////
 
-package messaging
+package cmix
 
 import (
 	"crypto/sha256"
