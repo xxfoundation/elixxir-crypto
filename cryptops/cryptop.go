32e450ac96306822f6ebec3976181c8fd4f2b968
Benjamin Wenger <ben@elixxir.ioo>
2019-04-05 16:39:34 -0700

    detailed the cryptops interface

--- cryptops/cryptop.go
+++ cryptops/cryptop.go
@@ -1,6 +1,9 @@
 package cryptops
 
 type Cryptop interface {
+	//Returns the name.  Used for debugging.
 	GetName() string
+	//Gets the number of parallel computations the cryptop does at once.
+	//A value of zero denotes it is arbitrary
 	GetInputSize() uint32
 }
