f5c989688eb7ef732f291ae9ec9e28c5a3ea505c
Benjamin Wenger <ben@elixxir.ioo>
2019-04-05 11:57:56 -0700

    replaced keyIve with key in cryptops.Elgamal

--- cryptops/elgamal.go
+++ cryptops/elgamal.go
@@ -2,20 +2,20 @@ package cryptops
 
 import "gitlab.com/elixxir/crypto/cyclic"
 
-type ElGamalPrototype func(g *cyclic.Group, keyInv, privatekey, publicCypherKey, ecrKeys, cypher *cyclic.Int)
+type ElGamalPrototype func(g *cyclic.Group, key, privatekey, publicCypherKey, ecrKeys, cypher *cyclic.Int)
 
 //Implements the modified version of ElGamal within the cryptops interface.
 //Modifies ecrkeys and cypher to make its output.
-//ecrkeys = ecrkeys*keyInv*(g^privatekey)%p
+//ecrkeys = ecrkeys*key*(g^privatekey)%p
 //cypher  = cypher*(publicCypherKey^privatekey)%p
 //More details can be found in the appendix of https://drive.google.com/open?id=1ha8QtUI9Tk_sCIKWN-QE8YHZ7AKofKrV
 //At Engineering/Technical Docs/CMIX.pdf
-var ElGamal ElGamalPrototype = func(g *cyclic.Group, keyInv, privateKey, publicCypherKey, ecrKeys, cypher *cyclic.Int) {
+var ElGamal ElGamalPrototype = func(g *cyclic.Group, key, privateKey, publicCypherKey, ecrKeys, cypher *cyclic.Int) {
 	tmp := g.NewMaxInt()
 
-	//ecrkeys = ecrkeys*keyInv*(g^privatekey)%p
+	//ecrkeys = ecrkeys*key*(g^privatekey)%p
 	g.ExpG(privateKey, tmp)
-	g.Mul(keyInv, tmp, tmp)
+	g.Mul(key, tmp, tmp)
 	g.Mul(tmp, ecrKeys, ecrKeys)
 
 	//cypher  = cypher*(publicCypherKey^privatekey)%p
