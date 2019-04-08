01e6269be034ae4fe2374e46ec9a1099ba2ea9e2
Spencer Brown <spencer@privategrity.com>
2019-04-08 13:33:32 -0700

    Add back old Shuffle functions for compatibility with server master

--- shuffle/shuffle.go
+++ shuffle/shuffle.go
@@ -14,8 +14,48 @@ import (
 	"encoding/binary"
 )
 
+// Legacy 64-bit shuffle implementation
+
 // Used in test to guarantee 100% coverage
-func shuffleCore(shufflee *[]uint32, rng csprng.Source) {
+func shuffleCore(shufflee *[]uint64, rng csprng.Source) {
+	size := uint64(len(*shufflee))
+	bufLen := uint64((bits.Len(uint(size-1))+7)>>3)
+	buf := make([]byte, bufLen)
+
+	for curPos := uint64(0); curPos < size-1; curPos++ {
+		// Shuffle should be able to swap with any element that hasn't
+		// already been shuffled
+		n, err := rng.Read(buf)
+		if err != nil || n != len(buf) {
+			jww.FATAL.Panicf("Could not generate random "+
+				"number in Shuffle: %v", err.Error())
+		}
+
+		buf = append(make([]byte,8-len(buf)),buf...)
+
+		// Generate a number between curPos and size-1
+		randPos := binary.BigEndian.Uint64(buf)
+		randPos %= size - curPos
+		randPos += curPos
+		(*shufflee)[randPos], (*shufflee)[curPos] = (*shufflee)[curPos],
+			(*shufflee)[randPos]
+	}
+}
+
+// Shuffles a uint64 array using a Fisher-Yates shuffle
+func Shuffle(shufflee *[]uint64) {
+	// Skip empty lists or lists of only 1 element, they cannot be shuffled
+	if len(*shufflee) <= 1 {
+		return
+	}
+	shuffleCore(shufflee, csprng.NewSystemRNG())
+}
+
+
+// New, shiny 32-bit shuffle implementation
+
+// Used in test to guarantee 100% coverage
+func shuffleCore32(shufflee *[]uint32, rng csprng.Source) {
 	if len(*shufflee) > math.MaxUint32 {
 		jww.ERROR.Panic("Too many items in the shuffled batch")
 	}
@@ -49,10 +89,10 @@ func shuffleCore(shufflee *[]uint32, rng csprng.Source) {
 }
 
 // Shuffles a uint32 array using a Fisher-Yates shuffle
-func Shuffle(shufflee *[]uint32) {
+func Shuffle32(shufflee *[]uint32) {
 	// Skip empty lists or lists of only 1 element, they cannot be shuffled
 	if len(*shufflee) <= 1 {
 		return
 	}
-	shuffleCore(shufflee, csprng.NewSystemRNG())
+	shuffleCore32(shufflee, csprng.NewSystemRNG())
 }
