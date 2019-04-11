3844ab08e01cfbe749af3c5733100032fe73e9ae
Konstantino Sparakis <ksparakis@elixxir.io>
2019-04-11 16:03:10 -0400

    returns value as well

--- cryptops/mul2.go
+++ cryptops/mul2.go
@@ -2,12 +2,11 @@ package cryptops
 
 import "gitlab.com/elixxir/crypto/cyclic"
 
-type Mul2Prototype func(g *cyclic.Group, ecr, key *cyclic.Int)
+type Mul2Prototype func(g *cyclic.Group, ecr, key *cyclic.Int) *cyclic.Int
 
-var Mul2 Mul2Prototype = func(g *cyclic.Group, ecr, key *cyclic.Int) {
-
+var Mul2 Mul2Prototype = func(g *cyclic.Group, ecr, key *cyclic.Int) *cyclic.Int{
 	g.Mul(ecr,key,ecr)
-
+	return ecr
 }
 
 //Returns the name for debugging
