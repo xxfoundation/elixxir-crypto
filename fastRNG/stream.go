1ca21ff6be51aa845a473238cfd0e7141e22feca
josh <josh@elixxir.io>
2019-07-05 17:09:28 -0700

    moving things to new package!

---
+++ fastRNG/stream.go
@@ -0,0 +1,129 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+// Implementation of the Fortuna construction as specified by Feruson, Schnier and Kohno
+// in 'Cryptography Engineering: Design Principles and Practical Applications'
+// Link: https://www.schneier.com/academic/paperfiles/fortuna.pdf
+package csprng
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
+	"encoding/binary"
+	jww "github.com/spf13/jwalterweatherman/jwalterweatherman"
+	"sync"
+)
+
+//Global hashing variable, used in the Fortuna construction
+var globalHash = sha256.New()
+
+type StreamGenerator struct {
+	AESCtr        cipher.Stream
+	scalingFactor uint
+	entropyCnt    uint
+	rng           Source
+	src           []byte
+}
+
+type Stream struct {
+	streamGen *StreamGenerator
+	mutex     sync.Mutex
+}
+
+/* Different ticket, put here for my convenience
+// NewStreamGenerator creates a StreamGenerator object containing up to streamCount streams.
+func NewStreamGenerator(source Source, scalingFactor uint, streamCount uint) *StreamGenerator{
+	return &StreamGenerator
+}
+// GetStream gets an existing or creates a new Stream object. If the # of open streams exceeds streamCount,
+// this function blocks (and prints a log warning) until a stream is available
+func (*StreamGenerator) GetStream() *Stream{
+
+}
+// Close closes the stream object, locking it from external users and marking it as avaialble in the stream list
+func (*RNGStreamGenerator) Close(*RNGStream)
+*/
+
+// Read reads up to len(b) bytes from the csprng.Source object. This function panics if the stream is locked.
+// Users of stream objects should close them when they are finished using them. We read the AES256
+// blocksize into AES then run it until blockSize*scalingFactor bytes are read. Every time
+// blocksize*scalingFactor bytes are read this functions blocks until it rereads csprng.Source.
+// TODO: Add 'blocking' logic, which is blocked by the ticket currently described above
+func (s *Stream) Read(b []byte) int {
+	//s.mutex.Lock()
+	//If the requested buffer exceeds the randomness generated thus far, then append until we have enough
+	if len(b) > len(s.streamGen.src) {
+		s.AppendSource(len(b))
+	}
+
+	//Read from source
+	if requiredRandomness := s.requiredRandomness(uint(len(b))); requiredRandomness != 0 {
+		_, err := s.streamGen.rng.Read(s.streamGen.src[0:requiredRandomness])
+		if err != nil {
+			jww.ERROR.Printf(err.Error())
+		}
+		//
+		s.SetEntropyCount(uint(len(b)))
+	}
+
+	//
+	s.streamGen.entropyCnt -= uint(len(b))
+	//Make 'new randomness' by changing the stale values (already read data read through xor'ring
+	//We may also just as easily retire the read values. This is up to discussion?
+	s.streamGen.AESCtr.XORKeyStream(s.streamGen.src[:len(b)], b)
+	//s.mutex.Unlock()
+	return len(b)
+}
+
+// If the source is not large for the amount to be read in, extend the source
+// using the Fortuna construction. Need a new block IV every
+// In usage, src will initially pull from Linux's rng
+func (s *Stream) AppendSource(lenToApp int) {
+	//Initialize key and block
+	key := make([]byte, 0)
+	seedArr := append(key, s.streamGen.src...)
+	key = globalHash.Sum(seedArr)
+
+	block, err := aes.NewCipher(key[:aes.BlockSize])
+	if err != nil {
+		panic(err)
+	}
+	ciphertext := make([]byte, aes.BlockSize+len(key))
+	var temp uint16 = 0
+	counter := make([]byte, aes.BlockSize)
+	//Encrypt the key and counter, inc ctr for next round of generation
+	for len(s.streamGen.src) < lenToApp {
+		//Increment the temp, place in the counter. When the temp var overflows, the 1 is carried over to the next byte
+		//in counter, treating it like a binary number
+		binary.LittleEndian.PutUint16(counter, temp)
+		stream := cipher.NewCTR(block, counter)
+		stream.XORKeyStream(ciphertext[aes.BlockSize:], key)
+		//So there is no predictable iv appended to the random src
+		tmp := ciphertext[aes.BlockSize:]
+		s.streamGen.src = append(s.streamGen.src, tmp...)
+		temp++
+	}
+}
+
+// TODO: test this function
+// Sets the required randomness, ie the amount we will read from source by factoring in the amount of entropy we
+// actually have and the sources of entropy we have. Definitions:
+func (s *Stream) requiredRandomness(requestLen uint) uint {
+	//Such that (requestLen - entropyCnt is never negative
+	if s.streamGen.entropyCnt < requestLen {
+		//The addition (scalingFactor - 1) ensures that the returned value is always a ceiling rather than a floor
+		//as an integer. e.g ceiling(a/b) = (a+b-1)/b
+		return (requestLen - s.streamGen.entropyCnt + s.streamGen.scalingFactor - 1) / s.streamGen.scalingFactor
+	}
+	return 0
+}
+
+// Increases the entropy by a factor of the requestedLen
+//
+func (s *Stream) SetEntropyCount(requestedLen uint) {
+	s.streamGen.entropyCnt += requestedLen * s.streamGen.scalingFactor
+}
---
+++ fastRNG/stream_test.go
@@ -0,0 +1,113 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+package csprng
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+	"testing"
+)
+
+// Checking the functionality of appending the source
+// using the Fortuna construction
+func TestStreamRead(t *testing.T) {
+
+	//A large byte array, of which you will read size from src byte array
+	requestedBytes := make([]byte, 4125)
+
+	/*Initialize everything needed for stream*/
+
+	//Mock random source, of arbitrarily insufficient (small) size
+	testSource := make([]byte, 7, 7)
+	_, err := io.ReadFull(rand.Reader, testSource)
+	if err != nil {
+		panic(err.Error())
+	}
+	//Mock block cipher
+	testKey := make([]byte, 32)
+	testIV := make([]byte, aes.BlockSize)
+	block, err := aes.NewCipher(testKey[:aes.BlockSize])
+	if err != nil {
+		panic(err)
+	}
+	ciph := cipher.NewCTR(block, testIV)
+	//TODO: Replace with constructor after 2nd ticket is done
+	//Initialize streamGenerator
+	//sg := NewStreamGenerator(NewSystemRNG, scalingFactor 16, streamCount 2)
+	//sg.src = testSource
+	//sg.AESCtr = ciph
+	//Wont need to set entropy count
+	sg := &StreamGenerator{
+		src:           testSource,
+		entropyCnt:    24,
+		scalingFactor: 16,
+		AESCtr:        ciph,
+		rng:           NewSystemRNG(),
+	}
+
+	//Initialize the stream with the generator
+	stream := Stream{streamGen: sg}
+	stream.Read(requestedBytes)
+
+	if len(sg.src) < len(requestedBytes) || bytes.Compare(sg.src, testSource) == 0 {
+		t.Errorf("Fortuna construction did not add randomness to the source")
+	}
+}
+
+// Checking whether requiredRandomness returns zero when the entropyCount is less than the requestedLen
+func TestRequiredRandomness_ReturnsZero(t *testing.T) {
+	//Initialize a streamGenerator and stream
+	sg := &StreamGenerator{
+		entropyCnt:    24,
+		scalingFactor: 16,
+		rng:           NewSystemRNG(),
+	}
+	stream := Stream{streamGen: sg}
+	//Try to read less that the amount of entropy
+	var lessThanEntropy uint = 23
+	requiredRandomness := stream.requiredRandomness(lessThanEntropy)
+	//Since we are reading less than entropy, reqLen-entropy<0, in which case we return 0
+	//This is tested
+	if requiredRandomness != 0 {
+		t.Errorf("Required randomness is not being calculated correctly")
+	}
+
+}
+
+func TestRequiredRandomness_ReturnsNonZero(t *testing.T) {
+	//Initialize a streamGenerator and stream
+	sg := &StreamGenerator{
+		entropyCnt:    24,
+		scalingFactor: 16,
+		rng:           NewSystemRNG(),
+	}
+	stream := Stream{streamGen: sg}
+	//Try to read more that the amount of entropy
+	var greaterThanEntropy uint = 25
+	requiredRandomness := stream.requiredRandomness(greaterThanEntropy)
+	if requiredRandomness == 0 {
+		t.Errorf("Required randomness is not being calculated correctly")
+	}
+}
+
+func TestStream_SetEntropyCount(t *testing.T) {
+	sg := &StreamGenerator{
+		entropyCnt:    24,
+		scalingFactor: 16,
+		rng:           NewSystemRNG(),
+	}
+
+	stream := Stream{streamGen: sg}
+	stream.SetEntropyCount(2)
+	var testVal uint = 24 + 2*16
+
+	if sg.entropyCnt != testVal {
+		t.Errorf("Entropy count not reset correctly")
+	}
+}
