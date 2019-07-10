96a70f0604933b05fb499fad384bfb6d72971a1b
josh <josh@elixxir.io>
2019-07-10 15:13:58 -0700

    pls

--- fastRNG/stream.go
+++ fastRNG/stream.go
@@ -10,64 +10,29 @@
 package fastRNG
 
 import (
-	"crypto"
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/sha256"
 	"encoding/binary"
-	jww "github.com/spf13/jwalterweatherman/"
+	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/elixxir/crypto/csprng"
 	"sync"
 )
 
-<<<<<<< HEAD
-=======
 //Global hashing variable, used in the Fortuna construction
-var globalHash = sha256.New()
-<<<<<<< HEAD
->>>>>>> parent of d8055c7... more reviewing
-=======
->>>>>>> parent of d8055c7... more reviewing
 
 type StreamGenerator struct {
+	AESCtr        cipher.Stream
 	scalingFactor uint
+	entropyCnt    uint
+	rng           csprng.Source
+	src           []byte
 }
 
 type Stream struct {
 	streamGen *StreamGenerator
 	mutex     sync.Mutex
-	src           []byte
-	entropyCnt    uint
-	AESCtr        cipher.Stream
-	rng           csprng.Source
 }
-
-/* Different ticket, put here for my convenience
-// NewStreamGenerator creates a StreamGenerator object containing up to streamCount streams.
-func NewStreamGenerator(source Source, scalingFactor uint, streamCount uint) *StreamGenerator{
-	return &StreamGenerator
-}
-// GetStream gets an existing or creates a new Stream object. If the # of open streams exceeds streamCount,
-// this function blocks (and prints a log warning) until a stream is available
-func (*StreamGenerator) GetStream() *Stream{
-
-}
-// Close closes the stream object, locking it from external users and marking it as avaialble in the stream list
-func (*RNGStreamGenerator) Close(*RNGStream)
-*/
-
-/* Different ticket, put here for my convenience
-// NewStreamGenerator creates a StreamGenerator object containing up to streamCount streams.
-func NewStreamGenerator(source Source, scalingFactor uint, streamCount uint) *StreamGenerator{
-	return &StreamGenerator
-}
-// GetStream gets an existing or creates a new Stream object. If the # of open streams exceeds streamCount,
-// this function blocks (and prints a log warning) until a stream is available
-func (*StreamGenerator) GetStream() *Stream{
-
-}
-// Close closes the stream object, locking it from external users and marking it as avaialble in the stream list
-func (*RNGStreamGenerator) Close(*RNGStream)
-*/
 
 // Read reads up to len(b) bytes from the csprng.Source object. This function panics if the stream is locked.
 // Users of stream objects should close them when they are finished using them. We read the AES256
@@ -77,25 +42,25 @@ func (*RNGStreamGenerator) Close(*RNGStream)
 func (s *Stream) Read(b []byte) int {
 	//s.mutex.Lock()
 	//If the requested buffer exceeds the randomness generated thus far, then append until we have enough
-	if len(b) > len(s.src) {
+	if len(b) > len(s.streamGen.src) {
 		s.extendSource(len(b))
 	}
 
 	//Read from source
 	if requiredRandomness := s.getEntropyNeeded(uint(len(b))); requiredRandomness != 0 {
-		_, err := s.rng.Read(s.src[0:requiredRandomness])
+		_, err := s.streamGen.rng.Read(s.streamGen.src[0:requiredRandomness])
 		if err != nil {
 			jww.ERROR.Printf(err.Error())
 		}
-		//
+
 		s.SetEntropyCount(uint(len(b)))
 	}
 
 	//
-	s.entropyCnt -= uint(len(b))
+	s.streamGen.entropyCnt -= uint(len(b))
 	//Make 'new randomness' by changing the stale values (already read data read through xor'ring
 	//We may also just as easily retire the read values. This is up to discussion?
-	s.AESCtr.XORKeyStream(s.src[:len(b)], b)
+	s.streamGen.AESCtr.XORKeyStream(s.streamGen.src[:len(b)], b)
 	//s.mutex.Unlock()
 	return len(b)
 }
@@ -105,36 +70,31 @@ func (s *Stream) Read(b []byte) int {
 // In usage, src will initially pull from Linux's rng
 func (s *Stream) extendSource(extensionLen int) {
 	//Initialize key and block
-<<<<<<< HEAD
-<<<<<<< HEAD
-	var fortunaHash = crypto.SHA256
-	key := fortunaHash.New().Sum(s.src)
-	key = key[len(s.src):]
-=======
+	var globalHash = sha256.New()
 	key := globalHash.Sum(s.streamGen.src)
->>>>>>> parent of d8055c7... more reviewing
-=======
-	key := globalHash.Sum(s.streamGen.src)
->>>>>>> parent of d8055c7... more reviewing
+	key = key[len(s.streamGen.src):]
 
 	block, err := aes.NewCipher(key[:aes.BlockSize])
 	if err != nil {
 		jww.ERROR.Println(err)
 	}
+	//Make sure the key is the key size (32 bytes), panic otherwise
+	if len(key) != 32 {
+		panic("The key is not the correct length (ie not 32 bytes)!")
+	}
 	aesRngBuf := make([]byte, aes.BlockSize+len(key))
 	var temp uint16 = 0
 	counter := make([]byte, aes.BlockSize)
 	//Encrypt the key and counter, inc ctr for next round of generation
-	for len(s.src) < extensionLen {
+	for len(s.streamGen.src) < extensionLen {
 		//Increment the temp, place in the counter. When the temp var overflows, the 1 is carried over to the next byte
-		//in counter, treating it like a binary number
+		//in counter, treating it like a binary number. Counter is used as the IV
 		binary.LittleEndian.PutUint16(counter, temp)
-		//The counter is used as the IV
 		stream := cipher.NewCTR(block, counter)
 		stream.XORKeyStream(aesRngBuf[aes.BlockSize:], key)
 		//So there is no predictable iv appended to the random src
 		tmp := aesRngBuf[aes.BlockSize:]
-		s.src = append(s.src, tmp...)
+		s.streamGen.src = append(s.streamGen.src, tmp...)
 		temp++
 	}
 }
@@ -145,17 +105,17 @@ func (s *Stream) extendSource(extensionLen int) {
 func (s *Stream) getEntropyNeeded(requestLen uint) uint {
 	//Such that the return value is never negative (requestedLen - entropyCnt) would be negative
 	// if entropyCnt > requestedLen
-	if s.entropyCnt >= requestLen {
+	if s.streamGen.entropyCnt >= requestLen {
 		return 0
 	}
 
 	//The addition (scalingFactor - 1) ensures that the returned value is always a ceiling rather than a floor
 	//as an integer. e.g ceiling(a/b) = (a+b-1)/b
-	return (requestLen - s.entropyCnt + s.streamGen.scalingFactor - 1) / s.streamGen.scalingFactor
+	return (requestLen - s.streamGen.entropyCnt + s.streamGen.scalingFactor - 1) / s.streamGen.scalingFactor
 }
 
 // Increases the entropy by a factor of the requestedLen
-//
+//arghh
 func (s *Stream) SetEntropyCount(requestedLen uint) {
-	s.entropyCnt += requestedLen * s.streamGen.scalingFactor
+	s.streamGen.entropyCnt += requestedLen * s.streamGen.scalingFactor
 }
