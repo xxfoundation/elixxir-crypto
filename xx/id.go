5ef12b54786f044fab49332ab0e3484187c59d79
Richard T. Carback III <rick.carback@gmail.com>
2020-04-29 18:43:36 +0000

    Move util functions to a util file

--- xx/id.go
+++ xx/id.go
@@ -7,33 +7,11 @@
 package xx
 
 import (
-	"bytes"
 	"crypto/rsa"
-	"encoding/binary"
 	"github.com/pkg/errors"
 	"gitlab.com/elixxir/crypto/hash"
 	"gitlab.com/elixxir/primitives/id"
 )
-
-// IntToBytes converts an integer to big endian byte slice
-// Because int can be 32 or 64 bits, it is always treated
-// as 64 for the purposes of this function.
-func IntToBytes(x int) []byte {
-	byteBuf := new(bytes.Buffer)
-	// NOTE: binary.Write shouldn't ever error on any value of int64
-	// so we don't handle returned errors.
-	binary.Write(byteBuf, binary.BigEndian, int64(x))
-	return byteBuf.Bytes()
-}
-
-// PublicKeyBytes converts an RSA public key to a byte representation
-// Specifically N's bytes concatenated with the public exponent concatenated
-// at the end
-func PublicKeyBytes(key rsa.PublicKey) []byte {
-	pkBytes := IntToBytes(key.E)
-	pkBytes = append(pkBytes, key.N.Bytes()...)
-	return pkBytes
-}
 
 // NewID creates a new ID by hashing the public key with a random 256-bit number
 // and appending the ID type.
--- xx/id_test.go
+++ xx/id_test.go
@@ -14,28 +14,6 @@ import (
 	"testing"
 )
 
-func TestIntToBytes(t *testing.T) {
-	x := IntToBytes(1)
-	if byte(1) != x[7] {
-		t.Errorf("Int ToBytes: %d != %v", 1, x)
-	}
-
-	x = IntToBytes(-1)
-	for i := 0; i < 8; i++ {
-		if x[i] != 0xFF {
-			t.Errorf("IntToBytes: %d != %v", -1, x)
-		}
-	}
-
-	x = IntToBytes(65535)
-	for i := 0; i < 8; i++ {
-		if (i > 6 && x[i] != 0xFF) &&
-			(i <= 6 && x[i] != 0) {
-			t.Errorf("IntToBytes: %d != %v", -1, x)
-		}
-	}
-}
-
 func TestNewID(t *testing.T) {
 	// use insecure seeded rng to reproduce key
 	rng := rand.New(rand.NewSource(42))
