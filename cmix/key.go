58c0ba2da6c93cf9470180df217909955e1067cc
Sydney Anne Erickson <sydney@elixxir.io>
2020-05-11 12:00:17 -0700

    Update license information

--- cmix/key.go
+++ cmix/key.go
@@ -1,8 +1,8 @@
-////////////////////////////////////////////////////////////////////////////////
-// Copyright © 2020 Privategrity Corporation                                   /
-//                                                                             /
-// All rights reserved.                                                        /
-////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 xx network SEZC                                                       //
+//                                                                                        //
+// Use of this source code is governed by a license that can be found in the LICENSE file //
+////////////////////////////////////////////////////////////////////////////////////////////
 
 // Package cmix derives new keys within the cyclic group from salts and a base key.
 // It also is used for managing keys and salts for communication between clients
--- go.mod
+++ go.mod
@@ -3,15 +3,12 @@ module gitlab.com/elixxir/crypto
 go 1.13
 
 require (
-	github.com/golang/protobuf v1.3.3 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/golang/protobuf v1.4.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/jwalterweatherman v1.1.0
-	github.com/stretchr/testify v1.5.1 // indirect
-	gitlab.com/elixxir/primitives v0.0.0-20200218211222-4193179f359c
-	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d
-	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
-	google.golang.org/grpc v1.27.1
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	gitlab.com/elixxir/primitives v0.0.0-20200427224212-c067b44fcaa3
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
+	golang.org/x/sys v0.0.0-20200509044756-6aff5f38e54f // indirect
+	google.golang.org/grpc v1.29.1
 )
