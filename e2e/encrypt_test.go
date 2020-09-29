d362be55e7ac2d202d3d71b6d67d7bc679024fc9
josh <josh@elixxir.io>
2020-09-29 16:05:08 -0700

    Fix consistency test

--- e2e/encrypt_test.go
+++ e2e/encrypt_test.go
@@ -91,39 +91,39 @@ func TestEncryptDecrypt_LeadingZeroes(t *testing.T) {
 // (when replacing the random number generater with a pseudo one)
 func TestEncrypt_Consistency(t *testing.T) {
 	// Set up expected values with base64 encoding
-	expectedMsgs := []string{"Qg5IeGohcWaCM8cvXinWjfOa5h8z2r/t9escYdF0thDWkXh" +
-		"KlBcUroVBSV5fMXiTKgQ7GyquSu6cDEtD3XocscCr3sIewXkk+B2mPuJaOch7Gxm9MPh" +
-		"ZibCIyhKYZQCw4eYHEruLP/n+3muDVbjT2K7jV9MkRJkU37JTfm9XiBGEwZOiAx8r4f1" +
-		"kAX34ISgQoOlWfiIMYkxNDg9leOz8jo4JzFY2dKWYrdnqPWboTlYAm0dwH+dKo0zj69a" +
-		"hU0zZ1I9q2uVc65LyCqnCAUk0MwUYIuoYvNHm+yw5SjZWms2nK3mFgMevOYonJRkO2h2" +
-		"wusjlQoNv/5b+c/QfWWvfPiMTSwLakiwFqrtkIWqpDvePu2j7zYcngGMMLj1LhaJ6Vgy" +
-		"E/b357UxScPZWdBsFETq5wfX6AVGE8a785yrSLEQTtRBLDV+kaDHepzeirWL30txOSbi" +
-		"jNI6ZqdGud99g3pxNWVEvJe3rigBsx+rDSxCBCIkWeeyIbWH9VyNc159p0JuVXUQv6ix" +
-		"nlWQ04iCQ",
-		"MCEGyIYyNR0cE6W34+PRvJowhEWLw7n/eO0VDJYchG4syfzEidfE4QRcWydxiPxrNp5W" +
-			"3qmcoqLUKRgIse46+Ci3hi0gCDI2nf8sqqyCmhA78gJ2JBbor9tbdZiyoDQyxo+yNB6f" +
-			"OegtBXfjobc6fHmtRPyujBqyWOI7yZ+GbSfb9OwnFUP957D53FoK0AAxW5Rm1c4F7n8x" +
-			"WZ8SRQYxGTs2ZwZ+AyJcL340aklz3aB8TCNb69S8XiEktrEL0PGp83dkTXISyNABurLx" +
-			"GD9Axgyy3RxQFQwD7aKw8YNnf0GdZ30wXg8FCItz21WcvY6B7CslzJCd4xAZ+dBG9CHC" +
-			"6FcYXs9EHcm4jEVxXNUrY/93U1VaN3cisktcH2BIfzR6JEvgGhUpXyuKC0rQZTbjq/kd" +
-			"+5tanEnqc1PxDC0/eU2R4FQ4nQevQmwIY+b7UTL3jwvA5DvqXyFwJqU/u+4I9Q1UzttB" +
-			"d2o2jd8yfKnbrHYWRHi4VPoW7aKyQMx/b/K8hmGj2BRHMlGt3xjm6hlT",
-		"gB+zC+aZY/xL27WLwxMc6n+mmTRfih0Q7LCTeAbgIYUmJaLO5xLiVb6Pu4HXekz2i7Bl" +
-			"4N2BqOAgbm+0XJW773G/W3wfvXa2DVI57a47MOId0CcHhzcWGyOFDevQLiePwC/LXTE+" +
-			"R2X9FeCxG7a2RHEygziuMRgo6xXGLSJH/HCpbNTKpoVR+fbbBTDlOpKUc1jDQWFILnMm" +
-			"gq38mI2RaJTaumYvjUTZNCFRexTEh6omwdPBlrLmShm6ShFaOrcLRhQt2Ae9DZOG8x8A" +
-			"YaoSspIwlHABnDY76Oyd3gyVZFNW3BmDjg+nP/PuzzvLCFv43dXn0YvCHHMb7DDjdYOY" +
-			"SS/q3bsinJNvpeNNLWEiXJptNTtwUe4RXBB80a2Omj8mMTFikqyfI6bROlud8kIC+Jot" +
-			"P7gUA/OqK7q0ygnLDfguWD8YVn/WXUz2i6dNj4Kqq6zvjJfhVTfc/s98aKQNyXRWu4Cb" +
-			"icFB+k8hq7A69xlVvyAJBVEEWHAGTFRJlw9nIyunzRrwRzNHzw41ix6z",
-		"TPYY8KiJH22Pr25bB9m/DEgBFs5CKLlMtgbEubki10F1tFKIF17MJ48yjBgU72vyzTl+" +
-			"9LM3Ic1S87r6sHTda66zSQ/jzAn93na1SFLiVqZSE/kzZMrErqHhjjKTw4XFxHDZbTL4" +
-			"avMbJEkNHRxm2+6O8alFNA2ypGGO6F3S9FwwSzTrBkP85cVJTsQ4Uie5PCDAXgGuc+ru" +
-			"lRwvmaw9rXnpk/Oqtcgn3ojK245gI7AAN6vFxxxlnq3QoLkBl9Vq7IT7Elec8EIWrE/G" +
-			"7d3SCCMUFlRQ3rCVgxJ72ZLAqjsEjQPLMrxKplgDg20PurhjmOJ4olMx4tJ4DYHYoJTW" +
-			"5ouMKfdbkoblGgz8UrRu7ZMDaorSPU6Uq0MlzJ9V3iBJSEf6Z98m0Jn4UQMxdrpHplkc" +
-			"lb0jlGmcGPW2zqbiqgbmqezmqXj5lqFSOEjThX0JZ0iSDps9EwZZoIRExCMT/jdenILm" +
-			"Ve/wm8Wyr2f39bwlj7cHiN3cChc4nJz+UBEp05t2J2YVUrUqxdGkzKKQ",
+	expectedMsgs := []string{"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEfjIUiZFOoMt" +
+		"p3sak059svDbzLIWSxPXWZ3sYgpK5ImY4vOxyKinAQaoB2lSqNeENJQxOL9AfIPp6op/pRu" +
+		"jO92IyJRkhuriwO5STbbE7dPGhMQh8FbdY5NdZHgvC+PheVaiRjBl0omX9K/772/6LmiePS" +
+		"C2zQWr8AD9wCMKpu8UI0VzU5aEeSa6+LS2xhlhZ5YRPFl3CU/xBF+H3HS4pKVQW+l55NG/B" +
+		"FByu42/diSZvEX0V2BvgDqjnhKZnGqqZY6+L8sStHRqu+25UF7C0xE7yzwH2QELpmJ1Z39T" +
+		"Ht+eoM3slVgQ/PSgaOAoLOJUBLZpeorKHB9aeUZmy55JB+QNOFAlsAAA",
+		// Second element
+		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABxDYvU0XUQGV6d6tEis7PK0LUgXTdCXqNE0h8" +
+		"3IqpcV9p3bNjbb69OVL5HS6hVKyXmlE1oWMU2n+3h1tYw04drUgHBO0b33Hq+wNGOg+vwIny8" +
+		"iUNQAkkNcxHNwoMtOa81TOwJw+FFzKbTIYXA62ioCOYeFcpLzf0hQew4k42FAA9HWab+D6bsa7" +
+		"m6hxACRhzN8g8YbP2RzHGs3iq+YBpkNejCI2pRmzy+rY1FVQdWUl7Ww8jFzFnEvWRcp9VXboPn3" +
+		"4mN6RBsPwsgcO4HOEoTFcvgg77IpkcYX01MaGU0cwsIZ0vfBil48v6GJtDy3QY64bztwHQF3sjGC" +
+		"XoAYyGZZyWjXLUgAA",
+			// Third element
+		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+		"AAAAAAAAAAAAAAAAAAAAAAAAAAACaikAjDo5cPbfQLaxEUYgzukHf79kIuvl75/hSgowl5kXHUkYHg" +
+		"1PezcgZvQNWj32CkYJoCaXqYE8uJY8q/PWoJNyguHbktljoJMqC+sWZ1deM6CV4WdcwntQLfpbNAgm" +
+		"EQCQy0PTzVEiav71sLoUF+BlThekxOFnIkB5W/HBmO3gcpTVPfgqXGrqNjaOw8u6CZU/kZaYzCaaUT" +
+		"nzha/njg2QyV2mqutN7racgfu04p5GPJjFRgNclYx1je/ratq6rXDXEAsGXMvfWpZwVbBQAXxyiM3C3" +
+		"HjataoeCvaFYft4kCWB77hLbC5MFJA2mfon7DwmxvqOW3Wt3YfsD1g/6ZoIGhjGAAA",
+			// Fourth element
+		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+		"AAAAAAAAAAAAAAAAAAAEMiOohkEvDkAJFI16Ca0rLoBuBujPjKws9UqvEM0vhWQtje6y6eEvHfQSDgA0K/Hr" +
+		"xbZW5f+5g5f8K2dbtEZ2JizzxMJZ6Z54sB9OC0TVljf0u2WVKEJm600Zs2pn862ZPwaqw8IcfTL6g5YrcWZg" +
+		"OHWT0O0S/LduV8Go2oudbJ6nr5LPir/tUtQXY3Y0OpnusgZOOq7/vtMFDEigC7q+HLK0L4Qkq/V/AemW6mxGU" +
+		"0nBPqIDIjmw6ktXy/A38A/tTnTN+gBZZ0yP7ASPclSQt31Xn+IXzL0ButOPaYQy2kg2PbdrjpxUFylkz6SQCH" +
+		"lhc7JTF7n19kokLreNGvOhu/4C8YoPgAA",
 	}
 	// Generate keys and messages
 	var keys []*cyclic.Int
@@ -144,7 +144,7 @@ func TestEncrypt_Consistency(t *testing.T) {
 		expectedMsg, _ := b64.StdEncoding.DecodeString(expectedMsgs[i])
 
 		if !reflect.DeepEqual(encMsg, expectedMsg) {
-			t.Errorf("EncryptUnsafe() did not produce the correct message on test %v\n\treceived: %s\n\texpected: %#v", i, encMsg, expectedMsg)
+			t.Errorf("EncryptUnsafe() did not produce the correct message on test %v\n\treceived: %#v\n\texpected: %#v", i, encMsg, expectedMsg)
 			fmt.Println(b64.StdEncoding.EncodeToString(encMsg))
 		}
 	}
