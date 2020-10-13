f3db2191ed6bc55413246ac8ed730a54cbe0f6ec
josh <josh@elixxir.io>
2020-10-13 13:35:18 -0700

    Fix conflicting group generator

--- e2e/auth/keygen_test.go
+++ e2e/auth/keygen_test.go
@@ -8,9 +8,7 @@ package auth
 import (
 	"bytes"
 	"encoding/base64"
-	"gitlab.com/elixxir/crypto/cyclic"
 	"gitlab.com/elixxir/crypto/diffieHellman"
-	"gitlab.com/elixxir/crypto/large"
 	"math/rand"
 	"testing"
 )
@@ -117,22 +115,4 @@ func TestMakeAuthKey_InputVariance(t *testing.T) {
 		t.Errorf("Outputs generated were identical when the salt varied")
 	}
 
-}
-
-// Helper function which generate a group for testing
-func getGrp() *cyclic.Group {
-	primeString := "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1" +
-		"29024E088A67CC74020BBEA63B139B22514A08798E3404DD" +
-		"EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245" +
-		"E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED" +
-		"EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D" +
-		"C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F" +
-		"83655D23DCA3AD961C62F356208552BB9ED529077096966D" +
-		"670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B" +
-		"E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9" +
-		"DE2BCBF6955817183995497CEA956AE515D2261898FA0510" +
-		"15728E5A8AACAA68FFFFFFFFFFFFFFFF"
-	p := large.NewIntFromString(primeString, 16)
-	g := large.NewInt(2)
-	return cyclic.NewGroup(p, g)
 }
