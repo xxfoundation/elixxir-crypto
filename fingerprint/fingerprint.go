19cfb71b20ab3db6bc6b9b49d9ebffb79711e352
jbhusson <jonah@elixxir.io>
2021-01-13 11:40:26 -0500

    Make identity fp size constant, clean up return code

--- fingerprint/fingerprint.go
+++ fingerprint/fingerprint.go
@@ -14,10 +14,13 @@ import (
 	_ "golang.org/x/crypto/blake2b"
 )
 
+// Size of the identiy fingerprint defined in bits & converted to bytes for return type
+var identityFpSizeBits = 200
+var identityFpSizeBytes = identityFpSizeBits / 8
+
 // Create an identity fingerprint from encrypted message payload and recipient ID
 // Recipient ID is 200 bits and is the result of hashing the message payload with the marshalled ID
 func IdentityFP(encryptedMessagePayload []byte, recipientId *id.ID) ([]byte, error) {
-	fp := [25]byte{}
 	b2b := crypto.BLAKE2b_256.New()
 	_, err := b2b.Write(encryptedMessagePayload)
 	if err != nil {
@@ -29,6 +32,5 @@ func IdentityFP(encryptedMessagePayload []byte, recipientId *id.ID) ([]byte, err
 		return nil, errors.WithMessagef(err, "Failed to write recipient ID %+v to hash",
 			recipientId.Marshal())
 	}
-	_ = copy(fp[:], b2b.Sum(nil))
-	return fp[:], nil
+	return b2b.Sum(nil)[:identityFpSizeBytes], nil
 }
