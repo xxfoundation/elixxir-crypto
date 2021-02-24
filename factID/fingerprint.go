71dd3d1f3ed0d316c7663a4fce45387b6f8972f2
josh <josh@elixxir.io>
2021-02-24 15:39:48 -0800

    Change factSalt to a byte slice to avoid base64 decoding

--- factID/fingerprint.go
+++ factID/fingerprint.go
@@ -7,23 +7,18 @@
 package factID
 
 import (
-	"encoding/base64"
 	"gitlab.com/elixxir/primitives/fact"
 	"gitlab.com/xx_network/crypto/hasher"
 )
 
 // Salt used for fact hashing to prevent rainbow table attacks
-// This string is a base64 encoding of a randomly generated 32 byte slice
-var saltStr = "1DK2BBdsOb/2ml2uf5ARYK4a0Sj05+1zdXWjc50qQd8="
+var factSalt = []byte{212,50,182,4,23,108,57,191,246,154,93,174,127,144,17,96,174,26,209,40,244,231,237,115,117,117,163,115,157,42,65,223}
 
 // Creates a fingerprint of a fact
 func Fingerprint(f fact.Fact) []byte {
 	h := hasher.BLAKE2.New()
 	h.Write([]byte(f.Fact))
 	h.Write([]byte(f.T.Stringify()))
-	// Decode the base64 salt string. Error is suppressed due decoding a
-	// hard-coded string
-	salt, _ := base64.StdEncoding.DecodeString(saltStr)
-	h.Write(salt)
+	h.Write(factSalt)
 	return h.Sum(nil)
 }
