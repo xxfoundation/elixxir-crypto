233a33c420db403f807a0110446115ec5df211b9
josh <josh@elixxir.io>
2021-02-24 14:47:18 -0800

    Add a salt to fact hashing

--- factID/fingerprint.go
+++ factID/fingerprint.go
@@ -7,14 +7,23 @@
 package factID
 
 import (
+	"encoding/base64"
 	"gitlab.com/elixxir/primitives/fact"
 	"gitlab.com/xx_network/crypto/hasher"
 )
+
+// Salt used for fact hashing to prevent rainbow table attacks
+// This string is a base64 encoding of a randomly generated 32 byte slice
+var saltStr = "1DK2BBdsOb/2ml2uf5ARYK4a0Sj05+1zdXWjc50qQd8="
 
 // Creates a fingerprint of a fact
 func Fingerprint(f fact.Fact) []byte {
 	h := hasher.BLAKE2.New()
 	h.Write([]byte(f.Fact))
 	h.Write([]byte(f.T.Stringify()))
+	// Decode the base64 salt string. Error is suppressed due decoding a
+	// hard-coded string
+	salt, _ := base64.StdEncoding.DecodeString(saltStr)
+	h.Write(salt)
 	return h.Sum(nil)
 }
