95b0761d61fafacfb938b6863313d83e0d0335a2
Richard T. Carback III <rick.carback@gmail.com>
2022-01-10 17:00:06 +0000

    Revert "Revert "Merge branch 'dev' into 'release'""
    This reverts commit d6713e35c9d9f7222071dfecc9dbbdfc18e5851a.

--- e2e/singleUse/mac.go
+++ e2e/singleUse/mac.go
@@ -9,8 +9,8 @@ package singleUse
 
 import (
 	"bytes"
-	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/elixxir/crypto/hash"
+	"crypto/hmac"
 )
 
 const macSalt = "singleUseMacSalt"
@@ -18,18 +18,9 @@ const macSalt = "singleUseMacSalt"
 // MakeMAC generates the MAC used in both the transmission and response CMIX
 // messages.
 func MakeMAC(key []byte, encryptedPayload []byte) []byte {
-	// Create new hash
-	h, err := hash.NewCMixHash()
-	if err != nil {
-		jww.ERROR.Panicf("Failed to create new hash for single-use MAC: %v", err)
-	}
-
-	// Hash the key, encrypted payload, and salt
-	h.Write(key)
+	h := hmac.New(hash.DefaultHash, key)
 	h.Write(encryptedPayload)
 	h.Write([]byte(macSalt))
-
-	// Get hash bytes
 	mac := h.Sum(nil)
 
 	// Set the first bit as zero to ensure everything stays in the group
