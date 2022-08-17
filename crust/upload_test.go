813291c0f3ce38281569eca3779d9c8b9053c286
joshemb <josh@elixxir.io>
2022-08-17 12:46:51 -0700

    Write consistency tests

--- crust/upload_test.go
+++ crust/upload_test.go
@@ -7,7 +7,9 @@
 package crust
 
 import (
+	"encoding/base64"
 	"gitlab.com/xx_network/crypto/signature/rsa"
+	"reflect"
 	"testing"
 )
 
@@ -43,16 +45,78 @@ func TestSignVerifyUpload(t *testing.T) {
 		t.Fatalf("Failed to generate private key: %v", err)
 	}
 
+	// Sign and verify
 	for i := 0; i < numTests; i++ {
+		// Sign data
 		sig, err := SignUpload(notRand, privKey, files[i], timestamps[i])
 		if err != nil {
 			t.Fatalf("Failed to generate sig %d/%d: %v", i, numTests, err)
 		}
 
+		// Use signature provided above and verify
 		err = VerifyUpload(privKey.GetPublic(), files[i], timestamps[i], sig)
 		if err != nil {
 			t.Fatalf("Failed to verify signature for test %d/%v: %v", i, numTests, err)
 		}
+	}
+
+}
+
+// Unit test: Generate signatures using pre-canned data
+// and compare it against the expected pre-canned data.
+func TestSignUpload_Consistency(t *testing.T) {
+	// use insecure seeded rng to ensure repeatability
+	notRand := &CountingReader{count: uint8(0)}
+
+	// Generate files
+	files := make([][]byte, numTests)
+	for i := 0; i < numTests; i++ {
+		file := make([]byte, 2048)
+		notRand.Read(file)
+
+		files[i] = file
+	}
+
+	// Generate timestamps
+	timestamps := make([][]byte, numTests)
+	for i := 0; i < numTests; i++ {
+		ts := make([]byte, 8)
+		notRand.Read(ts)
+
+		timestamps[i] = ts
+	}
+
+	// Generate a private key
+	privKey, err := rsa.GenerateKey(notRand, 4096)
+	if err != nil {
+		t.Fatalf("Failed to generate private key: %v", err)
+	}
+
+	// Generate signatures
+	signatures := make([]string, numTests)
+	for i := 0; i < numTests; i++ {
+		// Sign data
+		notRand = &CountingReader{count: uint8(0)}
+		sig, err := SignUpload(notRand, privKey, files[i], timestamps[i])
+		if err != nil {
+			t.Fatalf("Failed to SignUpload for %d/%d: %v", i, numTests, err)
+		}
+
+		signatures[i] = base64.StdEncoding.EncodeToString(sig)
+	}
+
+	// Expected (pre-canned) output
+	expectedSignatures := []string{
+		"IK1RdMMBKP3WLwfV0217Y3ERZq61uA5IaHfoKTisunPLjCtnIt5NUHcNGgxCrPsVSygnvduhaAiPOFTk6abeer2oDo9n2PXaWh6GbM4Y/05htUY35NU7JPg+Rv47rbFVnuNRffRdUf7hU01xkaTXXB8YKUqQZJLJ9pqb5tPzoer5DbbJVEcG5mxnmyF3QnTEum+8mwQbGZU4c3PtX/p/CEmW1UcRXOU6pMKCnoU+hpmewR7Q0zGVTrDbHAFYC7bMtSnkML0ueqZSVlC7WYdK694LjZboHqhDAPcpAXoyhPRpI9/B6Hh1gd2fNd75jo92WY+vnxy9pH+00OlWGBP0UTGPQAkIHp3LoMWhNyW9uIeLhnm1185zaVRJ9vkMlna8U0wMffBdzHPisy+GVJH6t0ma5XNCLGPOgx60k4mczwgFu8N2+rGAIYDDZe7kTOhe3LD7WPVj1XbpRn3kMLt2n9iw5W84hS2tzUBIgZpnYMWV7zNTMe7Ao7XQuOcyeKOuBOpDj6xiOlFOSx3yvHE56yoD6kFR0+fjE+y8EJ8VeGoLwIL/mu4S4YIjknQy9FMb3/ubdBsb1IMjKfKj+NcKkYJCsFfWCxnjqDW4r2yU8E97hlBhPwJFJ9omqybnBRVKH90WBmiyl8GFS7cTpNX5+KmkSJqvYGzSkYuPPB9x1X8=",
+		"YSa19Y+/C2AaQuexWQ82B6Q6ud7ItadWksYNbzFwtGU/G97lBSWFhFKWx9qPqYRd/mrL7fGkX/nSAh8Al60bJlh64F9vaFcwDetuZB8sk6teuWrHJfeV14u+kDEyfHKpnmHI/Nzfikv//bkpHTCQV6GpajeVv9CdAa4GVCcbWvH34VP1dC7HVUDHuNx5gjhmBJYGBop8fjbviQ+buewEb3Q9fhao0e+ZY4x7VrJHsv5ue9aQGIja8L46gECAU54D30d+X5sdWnefwvkc5YBhqTzPRZk86xn9QQ0y+m0/ukQnpAIs4MAQTVd5vCtfhD7OiHMOXKy45o+V3MJ0Gg5p7PBufdeVJ27IjDC3xTvWzxao1rSFmzwNi6IKoGWG0jaayazJiM3/Cso4QJ33RRZLotmVU9jhnYpfnD1Ywosb5o3OAc6fDOpYyx4oNEdpR9CVdhJQWTKETO9fFxuViFH8Oqg03bG6h+WhRnDMPmlMSn03E1pRJ4X7wzmcLfudqQUPLPWmstOHlERuW//G+x6JrhwDqL6Mt0csp5PPs8l63k7oL145dIdVgaooiMxmc/2Qxkk3KuQQzH92qUidHtFFuUNO3dyzitBT0pwvcIyeVArmzl2/+sX7XEiLiMdjVC2KGkcZiw6D0n/rrZ9SXMkyRdJcVx7eFTTyon+fXl2sl3w=",
+		"CN0NkNYVdsvXN4cfEpKwWBaqWkM07j5gruof7qM7e0awgVghU2XqeOHWsrb84zwtr5EMp0PO2glI5it5rPZbCPdEeNnOVOnVm+ZjrJiebkebOQDPPU0Fp7vD2STimUJdw9P1QRSFARdpBohshGdQc9rPpdWtx/qJFiv0D5dvx8dhzPC8vRuisONLuL0Sc0uURaEGxzqEBGxbo0yFlBf7c89Hqg/teankXYeu6W68I6vv0rVWa3eyRpiVU5nIOoW0wm3L6VzmlN0BkSWmV/cP2XC8sMbGZCpqfbCvZ4x8KtzIkDaNsNXC/+Gy3a8Xe/iMEBDQ7rX38regHxlk2zJpzyZZBt8ZGi2mLJ+rcwa16MXpGB6unRNlyUZiw1TxTYHFSb0KXhH0xOAzVxzpDxdx/jsiK9zz2IfIbW2vnjIMoHsDirvwefYJTohE0UDKLnP0tUmPrBUErHH1VC9atO6t/DjF2NTTW7CXq06EEkd9ToHJJ5NKcdxzeJ6id5hoAWvprZ6LIdTM+MiXdWNA2GHNhhzxYKGO/PH4qYRpw/7n053iQXYhqy9sYWQNzS+pjqII6lDaTiuZql5Cr93M69JaTxxIgakgyzFZnwVB8Gsl+akDD9J9Du4uPBeJe562jb7aOrI3KbG2Cb2KnQog3E9uuQs5egYhGI+Mbd11EVVYcRc=",
+	}
+
+	// Check generated output is consisted with pre-canned output
+	if !reflect.DeepEqual(expectedSignatures, signatures) {
+		t.Fatalf("Generated data does not match pre-canned data."+
+			"\nExpected: %v"+
+			"\nReceived: %v", expectedSignatures, signatures)
 	}
 
 }
--- crust/verification_test.go
+++ crust/verification_test.go
@@ -9,6 +9,7 @@ package crust
 import (
 	"encoding/base64"
 	"gitlab.com/xx_network/crypto/signature/rsa"
+	"reflect"
 	"testing"
 )
 
@@ -45,16 +46,77 @@ func TestSignVerifyVerification(t *testing.T) {
 
 	// Sign and verify
 	for i := 0; i < numTests; i++ {
+		// Sign data
 		sig, err := SignVerification(notRand, privKey,
 			usernames[i], receptionKeys[i])
 		if err != nil {
 			t.Fatalf("Failed to generate sig %d/%d: %v", i, numTests, err)
 		}
 
+		// Use signature provided above and verify
 		err = VerifyVerificationSignature(privKey.GetPublic(), usernames[i], receptionKeys[i], sig)
 		if err != nil {
 			t.Fatalf("Failed to verify signature for test %d/%v: %v", i, numTests, err)
 		}
+	}
+
+}
+
+// Unit test: Generate signatures using pre-canned data
+// and compare it against the expected pre-canned data.
+func TestSignVerification_Consistency(t *testing.T) {
+	// use insecure seeded rng to ensure repeatability
+	notRand := &CountingReader{count: uint8(0)}
+
+	// Generate usernames
+	usernames := make([]string, numTests)
+	for i := 0; i < numTests; i++ {
+		username := make([]byte, 25)
+		notRand.Read(username)
+
+		usernames[i] = base64.StdEncoding.EncodeToString(username)
+	}
+
+	// Generate reception keys
+	receptionKeys := make([][]byte, numTests)
+	for i := 0; i < numTests; i++ {
+		receptionKey := make([]byte, 32)
+		notRand.Read(receptionKey)
+		receptionKeys[i] = receptionKey
+	}
+
+	// Generate a private key
+	privKey, err := rsa.GenerateKey(notRand, 4096)
+	if err != nil {
+		t.Fatalf("Failed to generate private key: %v", err)
+	}
+
+	// Generate signatures
+	signatures := make([]string, numTests)
+	for i := 0; i < numTests; i++ {
+		// Sign data
+		notRand = &CountingReader{count: uint8(0)}
+		sig, err := SignVerification(notRand, privKey,
+			usernames[i], receptionKeys[i])
+		if err != nil {
+			t.Fatalf("Failed to generate sig %d/%d: %v", i, numTests, err)
+		}
+		signatures[i] = base64.StdEncoding.EncodeToString(sig)
+
+	}
+
+	// Expected (pre-canned) output
+	expectedSignatures := []string{
+		"V8hcPUqNp/ctKHXTCzcEBr/xi80vGTW3DUo1y0wHLpIGSOQ92QQI1wwm0sTE+CkhEB7qCN+4Yw63sqY+GJCEK67blEJjCTqNPRgE80TrMJ5sS3Q3geu6d5H6pYp+fqBI6whMBsb3+jzrepTIv5cLh8utSlZVSJPwIe7XwhAjk/rDBb4KcgD2SvqO58NAokb3aCVynINm1V83jfbiqLbXbGOGsE2OjGcApJIXJuZP+8BXZiCo8ydS0CE71hu/HcPKsoxekzYp0dCeSblfyRo+kNpFvSuDYTCcltz45d0amMc2mCkcgNcSdPE5mOMYv6FEwUIACZCzb55BFEliLV+9Ku7WCr1FrlCrOfTAlUs6+Yg+W81iw2Yow1DGwflf3TBKUW4aNDHr/Z6UyRsi3EN3tH2SnkNnHzNF0BsrhYqYLfJt0U64RLlMTLfE/p6lLDxsF2/FVt9s59xHXa5DAWY1vQDC7RaOXtmX1Ktg1C31bKlMzv+XWGYsgpCUXv7b+1Ux03U46jyA2Bci4N+tLJXr6J/MWN4XXfV1f3SsM3oGHBrEgkfVrtIZO84Mqo6IPj1VI3VHEsWbpFg558KJhz62Cg+mVNWWW21xUgo2912JEta4/S5D+grFpw/oQWY/sBOsab/hDklBt49mg35uFDj68vdB50ANb511Tt5bPnI/dis=",
+		"SughH7pFnqKtXWF0qMwBGmkrqGiR2Eu3G1Yq/PHjna0D3tdTEo6+gsK4tQYyswrqesvR2gahxoRYGoin0JAHBX52QLAPL2ncHBksOPr7IYtCK03SpE4sBLzAp9SWDhR51GgnTsiP3UfuyVgjCsbA8qkMGOL6vRIYLKyYIIEOb0e+QtA7eb/3QVhvwA/JzqAocOEAnmgDOuebFMEbQFfvR5t7rWqTnht277eA63ZtmTeK7jsCydNi82NHAZv8TlYq544EBGrajXQ/3shF44LGiBlEysUBl5BMuHUS7ZPQCArzyFuXQqnsEUiOmnVUS3K9tThNuQtwcDY9mJ44fgsRt+RhZ9PEtggLXHaZH6ShigvVa6nwnP2JEqikNHiDymA6uDjMr4MQDNjnqQhkIT9m11801Wb8o0z37yLCb0S16l3jEnCRmfqxbGNkEPHp2QZJD1h/NcVPiaeAgVJqNBq7Tn90vAv1H3pFk5VJUAUikiEdClzOPicrpX24BNPNo5ydpv7w1L1ic4I/k2ySQZ14/Ag4pUHIAyeaZY2CTyKKUk/6W3LZ1/il7fCmT4a5Y7orliGMLAW+llb/kuM7iwSjFaz8V9TGHzxDEQePkj6ftM4uAWIPswVQ29JUCyVv5jPrnO7MWfi4owj7FRrfmq2rc7HLy/scTTx2YCXa407n230=",
+		"cc2H6r4PzEfiAHg9Nzu/HpEgff0eklxE9j/s3F39oqqhjh6eH/X+avZ04QUDuDSNsCC5CiWngCC+BWfLhsZdxva3SZQJXEjhGM/jGXkj4aHoJn0GfDIp9RczDfwpi3lXesFmAYMFfNrcU15gOuFfFvaJx4pFuDhqmePpTyVYSv+KqXYRGYIQfzvKPFna8ZL0A3yiKCtFKg61vl6+MRkMj/R/klX9ndfLkBRc+80IWpgUJ8IIgGRnm60b6N4rnztJSKz6hXwMof5+8t4UVQ1MGJ9KS1ZLx4oMTb6HBeSG24y+uDAbb89OM6nc8gvrTX0n5Ok2fwbRT+AEFZf1CZkbaDUkg3mZ9GIIPyz8/rcq9R0W+tXYhvoiqZMwaHdv7Xiwz/TOgeuY+m7M/L/2aX8Tl6s4jLhUnD3KEW52yy/T2jgwaQtal1HkrbvyxHTNcJIf/M3IqERU50hJ8/2HuFhxIO/ywBOoHgj986ZelmXYGdLe9M3uP20beczJEHMt0GKvwNFeJronwK2f46c/pjAuXZcCMxNxuUQulBS/3exs8S8u1xg3UvYENC0NDqq28fga970tDlygEgYN4RkFg41mR2feseQ2lLYbGIXk7G2RRyhNkxuUD83vEVMjtOY6EyA+yWeE/f57MZZePK+JAUlg0AJ6PtJPMlRx2oVDN5+ahRg=",
+	}
+
+	// Check generated output is consisted with precanned output
+	if !reflect.DeepEqual(expectedSignatures, signatures) {
+		t.Fatalf("Generated data does not match pre-canned data."+
+			"\nExpected: %v"+
+			"\nReceived: %v", expectedSignatures, signatures)
 	}
 
 }
