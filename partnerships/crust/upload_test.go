685fb22a0e53e8aada0f0af94d45c75b21b4d73c
Benjamin Wenger <ben@elixxir.ioo>
2022-09-01 13:40:34 -0700

    added jointVerify and improved the interfaces and tweaked the tests

--- partnerships/crust/upload_test.go
+++ partnerships/crust/upload_test.go
@@ -74,8 +74,10 @@ func TestSignVerifyUpload(t *testing.T) {
 			t.Fatalf("Failed to generate sig %d/%d: %v", i, numTests, err)
 		}
 
+		fileHash, _ := hashFile(files[i])
+
 		// Use signature provided above and verify
-		err = VerifyUpload(privKey.GetPublic(), now, timestamps[i], files[i], sig)
+		err = VerifyUpload(privKey.GetPublic(), now, timestamps[i], fileHash, sig)
 		if err != nil {
 			t.Fatalf("Failed to verify signature for test %d/%v: %v", i, numTests, err)
 		}
