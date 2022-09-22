b2ce2fd617d658ab9b50f5aa224ff0ec255583cf
joshemb <josh@elixxir.io>
2022-09-22 15:38:20 -0700

    Add tests for channel/identity

--- channel/identity.go
+++ channel/identity.go
@@ -1,3 +1,9 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx network SEZC                                                       //
+//                                                                                        //
+// Use of this source code is governed by a license that can be found in the LICENSE file //
+////////////////////////////////////////////////////////////////////////////////////////////
+
 package channel
 
 import (
@@ -9,42 +15,42 @@ import (
 )
 
 type Language uint8
+
 const (
 	English Language = iota
 )
 
-
-type PrivateIdentity struct{
+type PrivateIdentity struct {
 	Privkey *ed25519.PrivateKey
 	Identity
 }
 
 // Marshal creates a write version of the private identity
-func(i PrivateIdentity)Marshal()[]byte{
-	return append([]byte{i.CodesetVersion},append(*i.Privkey,i.PubKey...)...)
+func (i PrivateIdentity) Marshal() []byte {
+	return append([]byte{i.CodesetVersion}, append(*i.Privkey, i.PubKey...)...)
 }
 
 // UnmarshalPrivateIdentity created a private identity from a marshaled version
-func UnmarshalPrivateIdentity(data []byte)(PrivateIdentity, error){
-	if len(data)!=ed25519.PrivateKeySize+ed25519.PublicKeySize+1{
+func UnmarshalPrivateIdentity(data []byte) (PrivateIdentity, error) {
+	if len(data) != ed25519.PrivateKeySize+ed25519.PublicKeySize+1 {
 		return PrivateIdentity{}, errors.New("data to unmarshal as a " +
 			"private identity is the wrong length")
 	}
 
 	version := uint8(data[0])
-	privkey := ed25519.PrivateKey(data[1:1+ed25519.PrivateKeySize])
-	pubKey :=  ed25519.PublicKey(data[1+ed25519.PrivateKeySize:])
+	privkey := ed25519.PrivateKey(data[1 : 1+ed25519.PrivateKeySize])
+	pubKey := ed25519.PublicKey(data[1+ed25519.PrivateKeySize:])
 
 	pi := PrivateIdentity{
 		Privkey:  &privkey,
-		Identity: constructIdentity(pubKey,version),
+		Identity: constructIdentity(pubKey, version),
 	}
 
 	return pi, nil
 }
 
 // Identity is the public object describing all aspects of a channel definition
-type Identity struct{
+type Identity struct {
 	PubKey ed25519.PublicKey
 
 	Honorific CodeNamePart
@@ -52,7 +58,7 @@ type Identity struct{
 	Noun      CodeNamePart
 
 	Codename  string
-	Color    string
+	Color     string
 	Extension string
 
 	CodesetVersion uint8
@@ -60,9 +66,9 @@ type Identity struct{
 
 // GenerateIdentity create a new channels identity from scratch and assigns
 // it a codename
-func GenerateIdentity(rng io.Reader)(PrivateIdentity, error){
+func GenerateIdentity(rng io.Reader) (PrivateIdentity, error) {
 	pub, priv, err := ed25519.GenerateKey(rng)
-	if err!=nil{
+	if err != nil {
 		return PrivateIdentity{}, err
 	}
 
@@ -75,29 +81,29 @@ func GenerateIdentity(rng io.Reader)(PrivateIdentity, error){
 }
 
 // ConstructIdentity creates a codename from an extant identity
-func ConstructIdentity(pub ed25519.PublicKey)Identity{
+func ConstructIdentity(pub ed25519.PublicKey) Identity {
 	h, _ := blake2b.New256(nil)
 
 	honorific := generateCodeNamePart(h, pub, honorificSalt, honorifics)
 	adjective := generateCodeNamePart(h, pub, adjectiveSalt, adjectives)
 	noun := generateCodeNamePart(h, pub, nounSalt, nouns)
 
-	if honorific.Generated!=""{
+	if honorific.Generated != "" {
 		adjective.Generated = strings.Title(adjective.Generated)
 	}
 
-	if honorific.Generated!="" || adjective.Generated!=""{
+	if honorific.Generated != "" || adjective.Generated != "" {
 		noun.Generated = strings.Title(noun.Generated)
 	}
 
 	i := Identity{
-		PubKey:    pub,
-		Honorific: honorific,
-		Adjective: adjective,
-		Noun:      noun,
-		Codename:  honorific.Generated + adjective.Generated + noun.Generated,
-		Color: generateColor(h,pub),
-		Extension: generateExtension(h,pub),
+		PubKey:         pub,
+		Honorific:      honorific,
+		Adjective:      adjective,
+		Noun:           noun,
+		Codename:       honorific.Generated + adjective.Generated + noun.Generated,
+		Color:          generateColor(h, pub),
+		Extension:      generateExtension(h, pub),
 		CodesetVersion: codesetv0,
 	}
 	return i
@@ -106,42 +112,42 @@ func ConstructIdentity(pub ed25519.PublicKey)Identity{
 // constructIdentity creates a codename from an extant identity for a given
 // version
 // currently, because there is only version 0, it only deals with version 0
-func constructIdentity(pub ed25519.PublicKey, codesetVersion uint8)Identity{
+func constructIdentity(pub ed25519.PublicKey, codesetVersion uint8) Identity {
 	h, _ := blake2b.New256(nil)
 
 	honorific := generateCodeNamePart(h, pub, honorificSalt, honorifics)
 	adjective := generateCodeNamePart(h, pub, adjectiveSalt, adjectives)
 	noun := generateCodeNamePart(h, pub, nounSalt, nouns)
 
-	if honorific.Generated!=""{
+	if honorific.Generated != "" {
 		adjective.Generated = strings.Title(adjective.Generated)
 	}
 
-	if honorific.Generated!="" || adjective.Generated!=""{
+	if honorific.Generated != "" || adjective.Generated != "" {
 		noun.Generated = strings.Title(noun.Generated)
 	}
 
 	i := Identity{
-		PubKey:    pub,
-		Honorific: honorific,
-		Adjective: adjective,
-		Noun:      noun,
-		Codename:  honorific.Generated + adjective.Generated + noun.Generated,
-		Color: generateColor(h,pub),
-		Extension: generateExtension(h,pub),
+		PubKey:         pub,
+		Honorific:      honorific,
+		Adjective:      adjective,
+		Noun:           noun,
+		Codename:       honorific.Generated + adjective.Generated + noun.Generated,
+		Color:          generateColor(h, pub),
+		Extension:      generateExtension(h, pub),
 		CodesetVersion: codesetVersion,
 	}
 	return i
 }
 
 // Marshal creates a write version of the identity
-func(i Identity)Marshal()[]byte{
-	return append([]byte{i.CodesetVersion},i.PubKey...)
+func (i Identity) Marshal() []byte {
+	return append([]byte{i.CodesetVersion}, i.PubKey...)
 }
 
 // UnmarshalIdentity created an identity from a marshaled version
-func UnmarshalIdentity(data []byte)(Identity, error){
-	if len(data)!=ed25519.PublicKeySize+1{
+func UnmarshalIdentity(data []byte) (Identity, error) {
+	if len(data) != ed25519.PublicKeySize+1 {
 		return Identity{}, errors.New("data to unmarshal as an identity is " +
 			"the wrong length")
 	}
@@ -149,6 +155,5 @@ func UnmarshalIdentity(data []byte)(Identity, error){
 	version := uint8(data[0])
 	pubkey := ed25519.PublicKey(data[1:])
 
-	return constructIdentity(pubkey,version), nil
+	return constructIdentity(pubkey, version), nil
 }
-
--- channel/identity_test.go
+++ channel/identity_test.go
@@ -1,7 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx network SEZC                                                       //
+//                                                                                        //
+// Use of this source code is governed by a license that can be found in the LICENSE file //
+////////////////////////////////////////////////////////////////////////////////////////////
+
 package channel
 
 import (
 	"gitlab.com/xx_network/crypto/csprng"
+	"reflect"
 	"testing"
 )
 
@@ -11,18 +18,88 @@ func TestConstructIdentity(t *testing.T) {
 	rng := &csprng.SystemRNG{}
 	codenames := make([]string, 0, numtests)
 
-	for i:=0;i<numtests;i++{
+	for i := 0; i < numtests; i++ {
 		id, _ := GenerateIdentity(rng)
-		codenames = append(codenames, id.Codename + "#" + id.Extension +
+		codenames = append(codenames, id.Codename+"#"+id.Extension+
 			id.Color)
 	}
 
-	for i:=0;i<numtests;i++{
-		for j:=i+1;j<numtests;j++{
-			if codenames[i]==codenames[j]{
+	for i := 0; i < numtests; i++ {
+		for j := i + 1; j < numtests; j++ {
+			if codenames[i] == codenames[j] {
 				t.Errorf("2 generated codenames are the same, %d vs %d",
 					i, j)
 			}
 		}
+	}
+}
+
+// Checks that PrivateIdentity.Marshal and UnmarshalPrivateIdentity are inverse
+// operations.
+func TestPrivateIdentity_MarshalUnmarshal(t *testing.T) {
+
+	rng := &csprng.SystemRNG{}
+	privateIdentity, _ := GenerateIdentity(rng)
+	marshalledData := privateIdentity.Marshal()
+
+	receivedIdentity, err := UnmarshalPrivateIdentity(marshalledData)
+	if err != nil {
+		t.Fatalf("UnmarshalPrivateIdentity error: %+v", err)
+	}
+
+	if !reflect.DeepEqual(receivedIdentity, privateIdentity) {
+		t.Fatalf("UnmarshalPrivateIdentity did not construct identical "+
+			"identity from original structure."+
+			"\nExpected: %v"+
+			"\nReceived: %v", privateIdentity, receivedIdentity)
+	}
+
+}
+
+// Checks that Identity.Marshal and UnmarshalIdentity are inverse
+// operations.
+func TestIdentity_MarshalUnmarshal(t *testing.T) {
+
+	rng := &csprng.SystemRNG{}
+	privateIdentity, _ := GenerateIdentity(rng)
+	expected := privateIdentity.Identity
+	marshalledData := expected.Marshal()
+
+	received, err := UnmarshalIdentity(marshalledData)
+	if err != nil {
+		t.Fatalf("UnmarshalIdentity error: %+v", err)
+	}
+
+	if !reflect.DeepEqual(expected, received) {
+		t.Fatalf("UnmarshalIdentity did not construct identical identity "+
+			"from orignal structure."+
+			"\nExpected: %v"+
+			"\nReceived: %v", expected, received)
+	}
+
+}
+
+// Error case: UnmarshalPrivateIdentity should error when receiving
+// byte data that is not of length
+// [ed25519.PrivateKeySize] + [ed25519.PublicKeySize] + 1.
+func TestUnmarshalPrivateIdentity_Error(t *testing.T) {
+	empty := []byte("too short")
+	_, err := UnmarshalPrivateIdentity(empty)
+	if err == nil {
+		t.Fatalf("Expected error case was not met. " +
+			"UnmarshalPrivateIdentity should error when receiving byte data " +
+			"that is too short.")
+	}
+}
+
+// Error case: UnmarshalIdentity should error when receiving
+// byte data that is not of length [ed25519.PublicKeySize] + 1.
+func TestUnmarshalIdentity_Error(t *testing.T) {
+	empty := []byte("too short")
+	_, err := UnmarshalIdentity(empty)
+	if err == nil {
+		t.Fatalf("Expected error case was not met. " +
+			"UnmarshalIdentity should error when receiving byte data " +
+			"that is too short.")
 	}
 }
