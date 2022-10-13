8c003c146c7ae6c196e097341dedad520e48c7d6
David Stainton <dstainton@elixxir.io>
2022-10-13 15:24:35 -0400

    dm: Add license notices and keys adapters

---
+++ dm/keys.go
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 xx network SEZC                                                       //
+//                                                                                        //
+// Use of this source code is governed by a license that can be found in the LICENSE file //
+////////////////////////////////////////////////////////////////////////////////////////////
+
+package dm
+
+import (
+	jww "github.com/spf13/jwalterweatherman"
+	"gitlab.com/elixxir/crypto/nike"
+	"gitlab.com/elixxir/crypto/nike/ecdh"
+	"gitlab.com/yawning/nyquist.git/dh"
+)
+
+func privateToNyquist(privKey nike.PrivateKey) dh.Keypair {
+	_, ok := privKey.(*ecdh.PrivateKey)
+	if !ok {
+		jww.FATAL.Panic("private key must be x25519 ECDH")
+	}
+
+	myPrivKey, err := protocol.DH.ParsePrivateKey(privKey.Bytes())
+	if err != nil {
+		jww.FATAL.Panic(err)
+	}
+
+	return myPrivKey
+}
+
+func publicToNyquist(pubKey nike.PublicKey) dh.PublicKey {
+	_, ok := pubKey.(*ecdh.PublicKey)
+	if !ok {
+		jww.FATAL.Panic("public key must be x25519 ECDH")
+	}
+	myPubKey, err := protocol.DH.ParsePublicKey(pubKey.Bytes())
+	if err != nil {
+		jww.FATAL.Panic(err)
+	}
+	return myPubKey
+}
