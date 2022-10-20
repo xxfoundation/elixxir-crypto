42831cb9dea25d86ff298ce75c67378ca8e7b59d
joshemb <josh@elixxir.io>
2022-10-20 14:41:03 -0700

    Add encryption system for speakeasky

---
+++ channel/database.go
@@ -0,0 +1,154 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file.                                                              //
+////////////////////////////////////////////////////////////////////////////////
+
+package channel
+
+import (
+	"github.com/pkg/errors"
+	jww "github.com/spf13/jwalterweatherman"
+	"gitlab.com/elixxir/crypto/hash"
+	"io"
+)
+
+const DatabaseSecretSize = 32
+
+// Crypt is the interface for storing encrypted channel messages into a
+// database.
+type Crypt interface {
+	Encrypt(raw []byte, standardEntryLength int) []byte
+	Decrypt(encrypted []byte) ([]byte, error)
+	GetSalt() []byte
+}
+
+// crypt is an internal structure which adheres to the Crypt interface.
+type crypt struct {
+	password []byte
+	salt     []byte
+	rng      io.Reader
+}
+
+// NewCrypt is a constructor which builds a Crypt. This will generate a
+// salt that will be used for key construction. This can be retrieved using
+// Crypt.GetSalt.
+func NewCrypt(internalPassword []byte, csprng io.Reader) Crypt {
+
+	salt := make([]byte, DatabaseSecretSize)
+	if _, err := io.ReadFull(csprng, salt); err != nil {
+		jww.FATAL.Panicf("Could not generate salt %+v", err)
+	}
+
+	return &crypt{
+		password: internalPassword,
+		salt:     salt,
+		rng:      csprng,
+	}
+}
+
+// Encrypt will encrypt the raw data. The standard entry length will be
+// minimum length that the message will be padded. This allows no information
+// about the encrypted message to be leaked at rest. To avoid padding the
+// message, simply pass in zero (0) as the standard entry length.
+func (c *crypt) Encrypt(raw []byte, standardEntryLength int) []byte {
+	raw = appendPadding(raw, standardEntryLength)
+
+	// Generate key
+	key := c.deriveDatabaseSecret()
+
+	// Generate cipher and nonce
+	chaCipher := initChaCha20Poly1305(key)
+	nonce := make([]byte, chaCipher.NonceSize())
+	if _, err := io.ReadFull(c.rng, nonce); err != nil {
+		jww.FATAL.Panicf("Could not generate nonce %+v", err)
+	}
+
+	// Encrypt data and return
+	ciphertext := chaCipher.Seal(nonce, nonce, raw, nil)
+	return ciphertext
+}
+
+// Decrypt will decrypt the passed in encrypted value. If the plaintext was
+// padded, the padding will be discarded at this level.
+func (c *crypt) Decrypt(encrypted []byte) ([]byte, error) {
+
+	// Generate key
+	key := c.deriveDatabaseSecret()
+
+	// Generate cypher
+	chaCipher := initChaCha20Poly1305(key)
+
+	nonceLen := chaCipher.NonceSize()
+	if (len(encrypted) - nonceLen) <= 0 {
+		return nil, errors.Errorf(readNonceLenErr, len(encrypted))
+	}
+
+	// The first nonceLen bytes of ciphertext are the nonce.
+	nonce, ciphertext := encrypted[:nonceLen], encrypted[nonceLen:]
+	paddedPlaintext, err := chaCipher.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, errors.Errorf(decryptWithPasswordErr, err)
+	}
+
+	plaintext := discardPadding(paddedPlaintext)
+
+	return plaintext, nil
+
+}
+
+// GetSalt retrieves the salt generated in the NewCrypt call.
+func (c *crypt) GetSalt() []byte {
+	return c.salt
+}
+
+// appendPadding is a helper function which adds padding to the raw plaintext,
+// if padding is necessary and specified.
+func appendPadding(raw []byte, standardEntryLength int) []byte {
+	if standardEntryLength == 0 {
+		jww.WARN.Printf("Standard entry length is zero, will not pad raw data!")
+	} else {
+		// Pad raw data if data is less than the standard length
+		difference := standardEntryLength - len(raw)
+		if difference > 0 {
+			dataToAppend := make([]byte, difference)
+			raw = append(raw, dataToAppend...)
+		}
+	}
+
+	return raw
+}
+
+// discardPadding is a helper function which will return the plaintext with the
+// padding removed.
+func discardPadding(data []byte) []byte {
+	// Starting from the tail and moving to the head, find the first index where
+	// the byte data is non-zero at said index
+	startOfPadding := len(data)
+	for i := len(data) - 1; i >= 0; i-- {
+		if data[i] != byte(0) {
+			break
+		}
+
+		startOfPadding = i
+
+	}
+
+	// The plaintext will be up to where padding starts
+	plaintext := data[:startOfPadding]
+
+	return plaintext
+}
+
+// deriveDatabaseSecret is a helper function which will generate the key for
+// encryption/decryption of
+func (c *crypt) deriveDatabaseSecret() []byte {
+	h, err := hash.NewCMixHash()
+	if err != nil {
+		jww.FATAL.Panicf("Failed to generate cMix hash: %+v", err)
+	}
+	h.Write(c.password)
+	h.Write(c.salt)
+	return h.Sum(nil)
+}
---
+++ channel/database_test.go
@@ -0,0 +1,162 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file.                                                              //
+////////////////////////////////////////////////////////////////////////////////
+
+package channel
+
+import (
+	"bytes"
+	"gitlab.com/xx_network/crypto/csprng"
+	"testing"
+)
+
+// TestCrypt_DiscardPadding_AllZeroes tests discardPadding.
+// Test Case: Data passed is all zeroes, meaning it should
+// all be considered padding.
+func TestCrypt_DiscardPadding_AllZeroes(t *testing.T) {
+	// Construct empty data (all 0's)
+	data := make([]byte, 32)
+
+	// Discard padding should return an empty byte slice
+	parsed := discardPadding(data)
+	if len(parsed) != 0 {
+		t.Errorf("Discard padding shoould have removed all "+
+			"data from %v", data)
+	}
+
+}
+
+// TestCrypt_DiscardPadding_NotAllZeroes tests discardPadding.
+// Test Case: Data passed is not all zeroes, meaning only all trailing zeroes
+// should be discarded.
+func TestCrypt_DiscardPadding_NotAllZeroes(t *testing.T) {
+	// Populate the first byte with non padding data
+	data := make([]byte, 32)
+	data[0] = 11
+
+	// Discard padding should return the first byte of data
+	parsed := discardPadding(data)
+	expected := []byte{11}
+	if !bytes.Equal(parsed, expected) {
+		t.Fatalf("discardPadding did not produce expected output."+
+			"\nExpected: %v"+
+			"\nReceived: %v", expected, parsed)
+	}
+}
+
+// TestCrypt_AppendPadding_NilCase tests appendPadding.
+// Test Case: Ensure that given nil and a non-zero entry length, an empty
+// byte slice of entry length is produced.
+func TestCrypt_AppendPadding_NilCase(t *testing.T) {
+	entryLength := 32
+	expected := make([]byte, entryLength)
+
+	received := appendPadding(nil, entryLength)
+	if !bytes.Equal(received, expected) {
+		t.Fatalf("appendPadding did not produce expected output."+
+			"\nExpected: %v"+
+			"\nReceived: %v", expected, received)
+	}
+
+}
+
+// TestCrypt_AppendPadding_SmallData tests appendPadding.
+// Test Case: Ensure that given a non-zero entry length and some data less
+// than entry length, appendPadding pads the data with zero data up to entry
+// length.
+func TestCrypt_AppendPadding_SmallData(t *testing.T) {
+	entryLength := 32
+	expected := make([]byte, entryLength)
+
+	smallData := []byte("123")
+	copy(expected, smallData)
+	received := appendPadding(smallData, entryLength)
+	if !bytes.Equal(received, expected) {
+		t.Fatalf("appendPadding did not produce expected output."+
+			"\nExpected: %v"+
+			"\nReceived: %v", expected, received)
+	}
+
+}
+
+// TestCrypt_AppendPadding_LargeData tests appendPadding.
+// Test Case: Ensure that given a non-zero entry length and some data larger
+// than entry length, appendPadding does not modify the data.
+func TestCrypt_AppendPadding_LargeData(t *testing.T) {
+	entryLength := 32
+
+	largeData := make([]byte, entryLength*2)
+
+	received := appendPadding(largeData, entryLength)
+	if !bytes.Equal(received, largeData) {
+		t.Fatalf("appendPadding should not modify data which is larger than the "+
+			"standard entry length (%d)."+
+			"\nExpected: %v"+
+			"\nReceived: %v", entryLength, largeData, received)
+	}
+
+}
+
+// TestCrypt_AppendPadding_ZeroEntryLength tests appendPadding.
+// Test Case: Ensure that given a zero value entry length, no padding occurs.
+func TestCrypt_AppendPadding_ZeroEntryLength(t *testing.T) {
+	entryLength := 0
+
+	expected := []byte("some random data")
+
+	received := appendPadding(expected, entryLength)
+
+	if !bytes.Equal(expected, received) {
+		t.Fatalf("appendPadding should not modify data when the standard entry "+
+			"length is 0."+
+			"\nExpected: %v"+
+			"\nReceived: %v", expected, received)
+	}
+
+}
+
+// TestCrypt_AppendPadding_ZeroEntryLength tests both appendPadding and
+// discardPadding.
+// Test Case: Ensure discardPadding will return the original data passed into
+// appendPadding.
+func TestCrypt_AppendPadding_DiscardPadding(t *testing.T) {
+	entryLength := 250
+	expected := []byte("expected data")
+
+	appendedData := appendPadding(expected, entryLength)
+	received := discardPadding(appendedData)
+
+	if !bytes.Equal(expected, received) {
+		t.Fatalf("discardPadding did not produce expected value."+
+			"\nExpected: %v"+
+			"\nRecieved: %v", expected, received)
+	}
+}
+
+// TestCrypt_EncryptDecrypt tests Crypt.Encrypt and Crypt.Decrypt.
+// Test Case: Ensure that Crypt.Decrypt returns the originally encrypted
+// plaintext when passed the output from Crypt.Encrypt.
+func TestCrypt_EncryptDecrypt(t *testing.T) {
+	password := []byte("test123")
+	rng := csprng.NewSystemRNG()
+
+	expected := []byte("original")
+	entryLength := 50
+	c := NewCrypt(password, rng)
+
+	encryptedData := c.Encrypt(expected, entryLength)
+	decryptedData, err := c.Decrypt(encryptedData)
+	if err != nil {
+		t.Fatalf("Decrypt returned an error: %+v", err)
+	}
+
+	if !bytes.Equal(expected, decryptedData) {
+		t.Fatalf("Crypt.Decrypt did not return originally encrypted data."+
+			"\nExpected: %v"+
+			"\nRecieved: %v", expected, decryptedData)
+	}
+
+}
