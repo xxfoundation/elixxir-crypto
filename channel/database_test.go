60093750283816088a156f8b09079a0ef227eafe
Jono Wenger <jono@elixxir.io>
2022-11-30 14:33:30 -0800

    Fix channel cipher comments

--- channel/database_test.go
+++ channel/database_test.go
@@ -14,110 +14,6 @@ import (
 	"testing"
 )
 
-// TestCrypt_DiscardPadding tests discardPadding.
-// Test Case: Basic unit test, ensuring that lengthOfPlaintext bytes are pulled
-// from the passed in byte data.
-func TestCrypt_DiscardPadding(t *testing.T) {
-	expected := []byte("123")
-	plaintextSizeBytes := make([]byte, lengthOfOverhead)
-	binary.PutUvarint(plaintextSizeBytes, uint64(len(expected)))
-
-	padding := make([]byte, 245)
-	data := append(plaintextSizeBytes, expected...)
-	data = append(data, padding...)
-
-	received := discardPadding(data)
-
-	if !bytes.Equal(expected, received) {
-		t.Fatalf("DiscardPadding did not output expected result."+
-			"\nExpected: %+v"+
-			"\nReceived: %+v", expected, received)
-	}
-
-}
-
-// TestCrypt_AppendPadding_SmallData tests appendPadding.
-// Test Case: Ensure that given a non-zero entry length and some data less
-// than entry length, appendPadding pads the data with zero data up to entry
-// length.
-func TestCrypt_AppendPadding_SmallData(t *testing.T) {
-	blockSize := 32
-	smallData := []byte("123")
-
-	// Use insecure seeded rng for reproducibility
-	notRand := &CountingReader{count: uint8(0)}
-
-	// Serialize length of plaintext
-	plaintextSizeBytes := make([]byte, lengthOfOverhead)
-	plaintextSize := len(smallData)
-	binary.PutUvarint(plaintextSizeBytes, uint64(plaintextSize))
-
-	// Construct padding
-	padding := make([]byte, blockSize-plaintextSize)
-	_, _ = notRand.Read(padding)
-	expected := append(plaintextSizeBytes, smallData...)
-	expected = append(expected, padding...)
-
-	received, err := appendPadding(
-		smallData, blockSize, &CountingReader{count: uint8(0)})
-	if err != nil {
-		t.Fatalf("appendPadding returned an unexpected error: %+v", err)
-	}
-	if !bytes.Equal(received, expected) {
-		t.Fatalf("appendPadding did not produce expected output."+
-			"\nExpected: %v"+
-			"\nReceived: %v", expected, received)
-	}
-
-}
-
-// TestCrypt_AppendPadding_ZeroEntryLength tests both appendPadding and
-// discardPadding.
-// Test Case: Ensure discardPadding will return the original data passed into
-// appendPadding.
-func TestCrypt_AppendPadding_DiscardPadding(t *testing.T) {
-	blockSize := 250
-	expected := []byte("expected data")
-
-	rng := csprng.NewSystemRNG()
-
-	appendedData, err := appendPadding(expected, blockSize, rng)
-	if err != nil {
-		t.Fatalf("appendPadding error: %+v", err)
-	}
-	received := discardPadding(appendedData)
-
-	if !bytes.Equal(expected, received) {
-		t.Fatalf("discardPadding did not produce expected value."+
-			"\nExpected: %v"+
-			"\nRecieved: %v", expected, received)
-	}
-}
-
-// TestCrypt_EncryptDecrypt tests Cipher.Encrypt.
-// Test Case: Ensure that Cipher.Encrypt returns an error if the plaintext
-// is larger than the pre-defined block size.
-func TestCipher_Encrypt_PlaintextTooLarge(t *testing.T) {
-	blockSize := 256
-
-	// Construct a plaintext larger than blockSize
-	plaintext := make([]byte, blockSize*2)
-
-	// Construct cipher
-	password := []byte("test123")
-	rng := csprng.NewSystemRNG()
-	salt := []byte("salt")
-	c, err := NewCipher(password, salt, blockSize, rng)
-	if err != nil {
-		t.Fatalf("Failed to create cipher: %+v", err)
-	}
-
-	_, err = c.Encrypt(plaintext)
-	if err == nil {
-		t.Fatalf("Encrypt should fail when plaintext is too large.")
-	}
-}
-
 // TestCrypt_EncryptDecrypt tests Cipher.Encrypt and Cipher.Decrypt.
 // Test Case: Ensure that Cipher.Decrypt returns the originally encrypted
 // plaintext when passed the output from Cipher.Encrypt.
@@ -148,6 +44,114 @@ func TestCrypt_EncryptDecrypt(t *testing.T) {
 			"\nRecieved: %v", expected, decryptedData)
 	}
 
+}
+
+// TestCrypt_EncryptDecrypt tests Cipher.Encrypt.
+// Test Case: Ensure that Cipher.Encrypt returns an error if the plaintext
+// is larger than the pre-defined block size.
+func TestCipher_Encrypt_PlaintextTooLarge(t *testing.T) {
+	blockSize := 256
+
+	// Construct a plaintext larger than blockSize
+	plaintext := make([]byte, blockSize*2)
+
+	// Construct cipher
+	password := []byte("test123")
+	rng := csprng.NewSystemRNG()
+	salt := []byte("salt")
+	c, err := NewCipher(password, salt, blockSize, rng)
+	if err != nil {
+		t.Fatalf("Failed to create cipher: %+v", err)
+	}
+
+	_, err = c.Encrypt(plaintext)
+	if err == nil {
+		t.Fatalf("Encrypt should fail when plaintext is too large.")
+	}
+}
+
+// Tests that a number of plaintexts with different block sizes can have padding
+// added via appendPadding and discarded via discardPadding and match the
+// original.
+func Test_appendPadding_discardPadding(t *testing.T) {
+	rng := csprng.NewSystemRNG()
+	tests := []struct {
+		plaintext []byte
+		blockSize int
+	}{
+		{[]byte("TestMessage"), 256},
+		{[]byte("Lorem Ipsum"), 16},
+		{bytes.Repeat([]byte("A"), 256), 256},
+		{bytes.Repeat([]byte("A"), 255), 256},
+		{[]byte{}, 256},
+		{[]byte{5}, 1},
+		{[]byte{}, 0},
+	}
+
+	for i, tt := range tests {
+		paddedData, err := appendPadding(tt.plaintext, tt.blockSize, rng)
+		if err != nil {
+			t.Fatalf("Failed to padd plaintext %d: %+v", i, err)
+		}
+
+		plaintext := discardPadding(paddedData)
+
+		if !bytes.Equal(tt.plaintext, plaintext) {
+			t.Fatalf("Encoded and decoded plaintext does not match expected."+
+				"\nexpected: %q\nrecieved: %v", tt.plaintext, plaintext)
+		}
+	}
+}
+
+// Ensure that given a non-zero entry length and some data less than entry
+// length, appendPadding pads the data with zero data up to entry length.
+func Test_appendPadding_SmallData(t *testing.T) {
+	blockSize := 32
+	smallData := []byte("123")
+
+	// Use insecure seeded rng for reproducibility
+	notRand := &CountingReader{count: uint8(0)}
+
+	// Serialize length of plaintext
+	plaintextSizeBytes := make([]byte, lengthOfOverhead)
+	plaintextSize := len(smallData)
+	binary.PutUvarint(plaintextSizeBytes, uint64(plaintextSize))
+
+	// Construct padding
+	padding := make([]byte, blockSize-plaintextSize)
+	_, _ = notRand.Read(padding)
+	expected := append(plaintextSizeBytes, smallData...)
+	expected = append(expected, padding...)
+
+	received, err := appendPadding(
+		smallData, blockSize, &CountingReader{count: uint8(0)})
+	if err != nil {
+		t.Fatalf("appendPadding returned an unexpected error: %+v", err)
+	}
+	if !bytes.Equal(received, expected) {
+		t.Fatalf("appendPadding did not produce expected output."+
+			"\nExpected: %v"+
+			"\nReceived: %v", expected, received)
+	}
+}
+
+// Tests that lengthOfPlaintext bytes are pulled from the passed in byte data.
+func Test_discardPadding(t *testing.T) {
+	expected := []byte("123")
+	plaintextSizeBytes := make([]byte, lengthOfOverhead)
+	binary.PutUvarint(plaintextSizeBytes, uint64(len(expected)))
+
+	padding := make([]byte, 245)
+	data := append(plaintextSizeBytes, expected...)
+	data = append(data, padding...)
+
+	received := discardPadding(data)
+
+	if !bytes.Equal(expected, received) {
+		t.Fatalf("DiscardPadding did not output expected result."+
+			"\nExpected: %+v"+
+			"\nReceived: %+v", expected, received)
+	}
 }
 
 type CountingReader struct {
