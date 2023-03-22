bcb4c100b7105793f3fb1596b04fce29a6f3d8e1
benjamin <ben@elixxir.io>
2023-03-22 16:02:57 -0700

    added the metadata field to the sealedBloom and used it in SIH

--- sealedBloomfilter/sealed.go
+++ sealedBloomfilter/sealed.go
@@ -15,17 +15,28 @@ import (
 
 var ErrNonceSize = errors.Errorf("nonce size must be %d", chacha20.NonceSizeX)
 var ErrKeySize = errors.Errorf("key size must be %d", chacha20.KeySize)
+var ErrInvalidMetadataLen = errors.Errorf("Metadata size is incorrect, " +
+	"does not match passed size")
+var ErrInvalidSealedLen = errors.Errorf("Sealed data size is incorrect, " +
+	"does not match the expected size")
 
 type Sealed interface {
-	// Seal generates a ciphertext of the the bit array only,
-	// excluding extraneous information of the bloom filter.
-	// Included for efficient DB storage purposes
-	Seal() ([]byte, error)
+	// Seal encrypts the filter in order to hide metadata, specifically
+	// the hamming weight of the filter. it returns the encrypted filter
+	// with the appended metadata inside the encrypted payload.
+	// Note: the length of the metadata must be the same as
+	// the passed size on initialization otherwise an error will
+	// be returned
+	Seal(metadata []byte) ([]byte, error)
 
-	// Unseal decrypts a ciphertext of the Sealed bloom filter it uses
-	// the bit array only, excluding extraneous information of the bloom
-	// filter and cipher which should be set through Init/InitByParams
-	Unseal(ciphertext []byte) error
+	// Unseal decrypted the sealed filter and stored it in the filter, returning
+	// the appended metadata.
+	// Will error if the passed in ciphertext is not the correct length of the
+	// filter+metadataSize. The size can be retrieved using sealed.SealedSize()
+	// Note: the length of the metadata must be the same as
+	// the passed size on initialization otherwise an error will
+	// be returned
+	Unseal(ciphertext []byte) ([]byte, error)
 
 	// Add adds the data to the ring of the bloom filter.
 	Add(data []byte)
@@ -47,8 +58,12 @@ type Sealed interface {
 	// Merge merges the sent Bloom into itself.
 	Merge(m Sealed) error
 
-	// Return the underlying, unencrypted bloom filter.
+	// Bloom Return the underlying, unencrypted bloom filter.
 	Bloom() *bloomfilter.Bloom
+
+	// SealedSize returns the size in bytes of a sealed payload this
+	// filter is expecting/ will return
+	SealedSize() int
 }
 
 type sealed struct {
@@ -56,6 +71,10 @@ type sealed struct {
 	// allow rollbacks that are needed for the unseal operation.
 	key   []byte
 	nonce []byte
+
+	// size of the optionally appendable metadata
+	metadataSize uint
+
 	// NOTE: we don't wrap here because we do not want to expose
 	// all the underlying functions, particularly the marshallers,
 	// which will not work as expected.
@@ -66,11 +85,14 @@ type sealed struct {
 // error. Given a number of elements, it accurately states if data is
 // not added. Within a falsePositive rate, it will indicate if the
 // data has been added.
+// metadataSize is the size of optional data appended to the bloomfilter
+// which is inside the seal and will be returned on unsealing.
+// It must be known a-priori by both sides
 //
 // WARNING: the (key, nonce) must never been repeated between bloom
 // filters, otherwise the seal can be trivially decrypted.
 func Init(key, nonce []byte, elements int,
-	falsePositive float64) (Sealed, error) {
+	falsePositive float64, metadataSize uint) (Sealed, error) {
 	if len(nonce) != chacha20.NonceSizeX {
 		return nil, ErrNonceSize
 	}
@@ -86,17 +108,22 @@ func Init(key, nonce []byte, elements int,
 		key:    key,
 		nonce:  nonce,
 		filter: *bloom,
+
+		metadataSize: metadataSize,
 	}, nil
 }
 
 // InitByParameters initializes a sealed bloom filter allowing the
 // user to explicitly set the size of the bit array and the amount of
 // hash functions
+// metadataSize is the size of optional data appended to the bloomfilter
+// which is inside the seal and will be returned on unsealing.
+// It must be known a-priori by both sides
 //
 // WARNING: the (key, nonce) must never been repeated between bloom
 // filters, otherwise the seal can be trivially decrypted.
 func InitByParameters(key, nonce []byte, size,
-	hashFunctions uint64) (Sealed, error) {
+	hashFunctions uint64, metadataSize uint) (Sealed, error) {
 	if len(nonce) != chacha20.NonceSizeX {
 		return nil, ErrNonceSize
 	}
@@ -109,17 +136,36 @@ func InitByParameters(key, nonce []byte, size,
 		return nil, err
 	}
 	return &sealed{
-		key:    key,
-		nonce:  nonce,
-		filter: *bloom,
+		key:          key,
+		nonce:        nonce,
+		filter:       *bloom,
+		metadataSize: metadataSize,
 	}, nil
 }
 
-func (s *sealed) Seal() ([]byte, error) {
+// Seal encrypts the filter in order to hide metadata, specifically
+// the hamming weight of the filter. it returns the encrypted filter
+// with the appended metadata inside the encrypted payload.
+// Note: the length of the metadata must be the same as
+// the passed size on initialization otherwise an error will
+// be returned
+func (s *sealed) Seal(metadata []byte) ([]byte, error) {
+	// check the metadata
+	if (s.metadataSize == 0 && metadata != nil) ||
+		uint(len(metadata)) != s.metadataSize {
+		return nil, ErrInvalidMetadataLen
+	}
+
+	// append the metadata to the marshaled filter
 	data, err := s.filter.MarshalStorage()
 	if err != nil {
 		return nil, err
 	}
+	if metadata != nil {
+		data = append(data, metadata...)
+	}
+
+	// seal the filter by encrypting it
 	ciphertext := make([]byte, len(data))
 	cipher, err := chacha20.NewUnauthenticatedCipher(s.key, s.nonce)
 	if err != nil {
@@ -129,14 +175,25 @@ func (s *sealed) Seal() ([]byte, error) {
 	return ciphertext, nil
 }
 
-func (s *sealed) Unseal(ciphertext []byte) error {
+// Unseal decrypted the sealed filter and stored it in the filter, returning
+// the appended metadata.
+// Will error if the passed in ciphertext is not the correct length of the
+// filter+metadataSize. The size can be retrieved using sealed.SealedSize()
+// Note: the length of the metadata must be the same as
+// the passed size on initialization otherwise an error will
+// be returned
+func (s *sealed) Unseal(ciphertext []byte) ([]byte, error) {
+	if len(ciphertext) != s.SealedSize() {
+		return nil, ErrInvalidSealedLen
+	}
 	plaintext := make([]byte, len(ciphertext))
 	cipher, err := chacha20.NewUnauthenticatedCipher(s.key, s.nonce)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	cipher.XORKeyStream(plaintext, ciphertext)
-	return s.filter.UnmarshalStorage(plaintext)
+	return plaintext[s.filter.BufferSize():],
+		s.filter.UnmarshalStorage(plaintext[:s.filter.BufferSize()])
 }
 
 func (s *sealed) Add(data []byte) {
@@ -165,4 +222,8 @@ func (s *sealed) Merge(m Sealed) error {
 
 func (s *sealed) Bloom() *bloomfilter.Bloom {
 	return &s.filter
+}
+
+func (s *sealed) SealedSize() int {
+	return int(s.metadataSize) + s.filter.BufferSize()
 }
