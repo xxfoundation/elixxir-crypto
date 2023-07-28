0145866ce3bfd4340b1cb7c792cf8b84a252b71e
jbhusson <jonah@elixxir.io>
2023-07-28 08:24:45 -0400

    Remove honorific

--- codename/channelCodename.go
+++ codename/channelCodename.go
@@ -7,15 +7,11 @@ import (
 
 func GenerateChannelCodename(input []byte) string {
 	h := hash.CMixHash.New()
-	honorific := generateCodeNamePart(h, input, honorificSalt, honorifics)
 	adjective := generateCodeNamePart(h, input, adjectiveSalt, adjectives)
 	noun := generateCodeNamePart(h, input, nounSalt, nouns)
-	if honorific.Generated != "" {
-		adjective.Generated = strings.Title(adjective.Generated)
-	}
 
-	if honorific.Generated != "" || adjective.Generated != "" {
+	if adjective.Generated != "" {
 		noun.Generated = strings.Title(noun.Generated)
 	}
-	return honorific.Generated + adjective.Generated + noun.Generated
+	return adjective.Generated + noun.Generated
 }
