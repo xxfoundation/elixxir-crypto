5edd768be49526d239949100be898730bc54bf7d
Richard T. Carback III <rick.carback@gmail.com>
2023-12-06 01:23:58 +0000

    Clean up the comments around the benger code using the language from the spec at git.xx.network/elixxir/docs/dm.md
    "benger code" is a deliberately made up name because it is deceptive to the reader to call it the message hmac as, although it is the same construction, it's not authenticating the message in the way one might expect.

--- dm/benger.go
+++ dm/benger.go
@@ -14,11 +14,13 @@ import (
 	"golang.org/x/crypto/blake2b"
 )
 
-// benger code is a partially deniable forgeable hmac. The hmac is forgeable
-// by the receiving party, making it so that it is impossible for the existence
-// of the code to tell a third party that a specific party created it, but
-// it is possible for the receiver to know the sender created it because they
-// know if they forged it or not
+// benger code is an hmac, but we do not call it that because it does
+// not authenticate with the key of the message. Instead, it proves
+// that the sender knows the private key of the public key included in
+// the message. This prevents a third party from sending someone elses
+// public key, making it appear to the user that the true the sender
+// of a given message is someone else. It is similar to the sender
+// signing the message they sent with their static public key.
 
 // isValidBengerCode checks if the benger code is valid
 func isValidBengerCode(readCode, key, msg []byte) bool {
@@ -31,10 +33,11 @@ func isValidBengerCode(readCode, key, msg []byte) bool {
 	return true
 }
 
-// makeBengerCode is a helper to create a simple keyed hash
-// This is the hash of a derived secret + message embedded in
-// a noise protocol message, which limits spoofed message sending
-// to the sender or receiver of the message.
+// makeBengerCode is a helper to create a simple keyed hash This is
+// the hash of a derived secret of both published public keys (not the
+// keys used to encryption the message) + message embedded in a noise
+// protocol message, which limits message sending to the sender or
+// receiver of the message.
 func makeBengerCode(key, msg []byte) []byte {
 	h, _ := blake2b.New256(nil)
 	h.Write(key)
